88d77df6ed2c0811af51f94a6ac10d092e3a966d
Stefano Maximiliano Antonel <stefano.antonel@epfl.ch>
2022-12-23 13:02:43 +0000

    Align Login / Logout btn

--- src/components/LoginButton/LoginButton.tsx
+++ src/components/LoginButton/LoginButton.tsx
@@ -3,22 +3,17 @@ import { unstable_getServerSession } from 'next-auth/next';
 import { SignInButton, SignOutButton } from './buttons';
 import { authOptions } from '@/pages/api/auth/[...nextauth]';
 
-import Styles from './login-button.module.css';
-
 export default async function LoginButton() {
   const session = await unstable_getServerSession(authOptions);
 
-  if (session?.user) {
-    return (
-      <div className={Styles.logoutButton}>
-        <SignInButton />
-
-        <div>
-          {session.user.name} ({session.user.username})
-        </div>
+  return !session?.user ? (
+    <SignInButton />
+  ) : (
+    <div>
+      <SignOutButton />
+      <div>
+        {session.user.name} ({session.user.username})
       </div>
-    );
-  }
-
-  return <SignOutButton />;
+    </div>
+  );
 }
--- src/components/LoginButton/buttons.tsx
+++ src/components/LoginButton/buttons.tsx
@@ -6,16 +6,16 @@ import Styles from './login-button.module.css';
 
 export function SignInButton() {
   return (
-    <button className={Styles.loginButton} type="button" onClick={() => signOut()}>
-      Logout
+    <button className={Styles.loginButton} type="button" onClick={() => signIn('keycloak')}>
+      Login
     </button>
   );
 }
 
 export function SignOutButton() {
   return (
-    <button className={Styles.loginButton} type="button" onClick={() => signIn('keycloak')}>
-      Login
+    <button className={Styles.loginButton} type="button" onClick={() => signOut()}>
+      Logout
     </button>
   );
 }
