97a83715d3b3cbd0c5b98ddd2c82d6477245ebe9
Pavlo Getta <pavlo.getta@epfl.ch>
2023-01-06 13:55:43 +0000

    Fix automatic login redirecting to a previous page

--- src/hooks/ensure-login.ts
+++ src/hooks/ensure-login.ts
@@ -2,18 +2,49 @@
 
 import { useEffect } from 'react';
 import { signIn, useSession } from 'next-auth/react';
+import { usePathname, useSearchParams } from 'next/navigation';
+
+/**
+ * Create an absolute URL (string) of the current page using pathname and searchParams
+ * from `next/navigation` component so that it's consistent with Next.js routing state.
+ * This is used for sign-in to always redirect the user to the current page.
+ *
+ * See a comment below for more details about the issue.
+ */
+function useCallbackUrl() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  if (!pathname || typeof window === 'undefined') return undefined;
+
+  const url = new URL(window.location.href);
+
+  url.pathname = pathname;
+  url.search = searchParams.toString();
+
+  return url.toString();
+}
 
 export default function useEnsureLogin() {
-  const { data: session, status } = useSession();
+  /*
+    Window.location used by next-auth to create default callbackUrl points to a previous
+    location while route change is pending, thus sign-in in some cases returns the user to a previous page.
+
+    To ovecome that using next/navigation to create a callbackUrl.
+  */
+  const callbackUrl = useCallbackUrl();
+
+  const { data: session } = useSession({
+    required: true,
+    onUnauthenticated() {
+      signIn('keycloak', { callbackUrl });
+    },
+  });
 
   useEffect(() => {
-    if (!session && status === 'unauthenticated') {
-      // automatically re-login when in a sub-route
-      signIn('keycloak');
-    }
     if (session?.error === 'RefreshAccessTokenError') {
       // automatically re-login if refresh token expires
-      signIn('keycloak');
+      signIn('keycloak', { callbackUrl });
     }
-  }, [session]);
+  }, [session, callbackUrl]);
 }
