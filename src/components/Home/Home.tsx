4cc91a10b8a331e283416c6ecac094c0a619270b
lolivier <loris.olivier@epfl.ch>
2023-02-02 10:15:55 +0100

    Changed the icon on home and the new title for the three sections

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -9,21 +9,21 @@ export default function Home() {
     <div className={styles.home}>
       <VideoPanel />
 
-      <SimplePanel className={Theme.colorPrimary9} title="Observatory" link="/observatory">
+      <SimplePanel className={Theme.colorPrimary9} title="Brain Observatory" link="/observatory">
         Explore a large collection of neuronal models, virtual simulations, and brain cell
         distribution in a 3D and interactive manner.
       </SimplePanel>
 
       <SimplePanel
         className={Theme.colorPrimary8}
-        title="Brain Factory"
+        title="Brain Lab"
         link="/brain-factory/load-brain-config"
       >
         Build your own brain configurations by customizing the cell compositions, assigning neuronal
         models and configuring the desired connectivity pattern.
       </SimplePanel>
 
-      <SimplePanel className={Theme.colorPrimary7} title="Virtual Lab" link="/virtual-lab">
+      <SimplePanel className={Theme.colorPrimary7} title="Brain simulation" link="/virtual-lab">
         Build your own brain configurations by customizing the cell compositions, assigning neuronal
         models and configuring the desired connectivity pattern.{' '}
       </SimplePanel>
