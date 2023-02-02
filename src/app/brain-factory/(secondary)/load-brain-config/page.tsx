221c72b762edae399ad3acc5bbdd7ebe4fc45166
Pavlo Getta <pavlo.getta@epfl.ch>
2023-02-02 15:46:58 +0100

    Apply basePath to redirect after a config is cloned

--- src/app/brain-factory/(secondary)/load-brain-config/page.tsx
+++ src/app/brain-factory/(secondary)/load-brain-config/page.tsx
@@ -1,10 +1,13 @@
 'use client';
 
+import { basePath } from '@/config';
 import BrainConfigLoaderView from '@/components/BrainConfigLoaderView';
 import useSessionState from '@/hooks/session';
 
 export default function LoadBrainConfigView() {
   useSessionState();
 
-  return <BrainConfigLoaderView baseHref="/brain-factory/cell-composition/interactive" />;
+  return (
+    <BrainConfigLoaderView baseHref={`${basePath}/brain-factory/cell-composition/interactive`} />
+  );
 }
