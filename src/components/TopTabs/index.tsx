841a54d88550439f5648c95c4828034c78a54778
Stefano Antonel <stefanoantonel@gmail.com>
2023-02-14 11:45:02 +0100

    Disable simulation button if the circuit was not built

--- src/components/TopTabs/index.tsx
+++ src/components/TopTabs/index.tsx
@@ -1,17 +1,28 @@
 import { useAtomValue } from 'jotai/react';
+import { loadable } from 'jotai/vanilla/utils';
 import { ErrorBoundary } from 'react-error-boundary';
 
+import { useEffect, useState } from 'react';
 import Tabs from '@/components/BrainFactoryTabs';
 import BuildModelBtn from '@/components/BuildModelBtn';
 import { themeAtom } from '@/state/theme';
 import WorkflowLauncherBtn from '@/components/WorkflowLauncherBtn';
 import { WORKFLOW_SIMULATION_TASK_NAME } from '@/services/bbp-workflow/config';
 import { SimpleErrorComponent } from '@/components/GenericErrorFallback';
+import { detailedCircuitAtom } from '@/state/brain-model-config/morphology-assignment';
 
 export default function TopTabs() {
   const theme = useAtomValue(themeAtom);
-
   const bgClassName = theme === 'light' ? 'bg-neutral-1' : 'bg-black';
+
+  const [circuitWasBuilt, setCircuitWasBuilt] = useState(false);
+  const detailedCircuitLoadable = useAtomValue(loadable(detailedCircuitAtom));
+
+  useEffect(() => {
+    if (detailedCircuitLoadable.state !== 'hasData') return;
+    if (!detailedCircuitLoadable.data) return;
+    setCircuitWasBuilt(true);
+  }, [detailedCircuitLoadable]);
 
   return (
     <div className={bgClassName}>
@@ -22,6 +33,7 @@ export default function TopTabs() {
             <WorkflowLauncherBtn
               buttonText="New in silico experiment"
               workflowName={WORKFLOW_SIMULATION_TASK_NAME}
+              disabled={!circuitWasBuilt}
             />
           </div>
         </Tabs>
