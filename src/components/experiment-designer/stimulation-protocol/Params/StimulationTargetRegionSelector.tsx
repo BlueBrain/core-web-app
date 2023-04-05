306de852effe25d0c7279d7c423c360d9b4270da
Stefano Maximiliano Antonel <stefano.antonel@epfl.ch>
2023-04-05 12:54:18 +0000

    Add Experiment Designer - Stimulation Protocol

---
+++ src/components/experiment-designer/stimulation-protocol/Params/StimulationTargetRegionSelector.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import { useSetAtom } from 'jotai';
+import { ExportOutlined } from '@ant-design/icons';
+
+import { expDesignerSimulateRegions } from '@/state/experiment-designer';
+import { BrainRegionsDropdown } from '@/components/experiment-designer';
+import { ExpDesignerRegionParameter } from '@/types/experiment-designer';
+import { classNames } from '@/util/utils';
+
+type Props = {
+  data: ExpDesignerRegionParameter;
+  className?: string;
+};
+
+export default function StimulationTargetRegionSelector({ data, className }: Props) {
+  const setSimulateRegions = useSetAtom(expDesignerSimulateRegions);
+
+  return (
+    <div className={classNames('flex gap-3 items-center font-bold', className)}>
+      <div className="grow">Target</div>
+      <BrainRegionsDropdown
+        onChange={setSimulateRegions}
+        defaultValue={data.value}
+        className={className}
+      />
+      <ExportOutlined />
+    </div>
+  );
+}
