9389078fe59e3e8c7ef463972a6434acd2482923
Gil Arturo Barrios Del Villar <gil.barriosdelvillar@epfl.ch>
2023-04-24 21:47:51 +0200

    type

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -1,9 +1,9 @@
 import { useEffect, useRef } from 'react';
 
-export default function usePrevious(value: any) {
+export default function usePrevious<T>(value: T) {
   // The ref object is a generic container whose current property is mutable ...
   // ... and can hold any value, similar to an instance property on a class
-  const ref = useRef();
+  const ref = useRef<T>();
   // Store current value in ref
   useEffect(() => {
     ref.current = value;
