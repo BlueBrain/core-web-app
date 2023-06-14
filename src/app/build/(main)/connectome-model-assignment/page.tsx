9f6628a373f32d55c314bf49d91abfb8f8065f83
Gil Arturo Barrios Del Villar <gil.barriosdelvillar@epfl.ch>
2023-06-14 22:19:34 +0200

    render rules

--- src/app/build/(main)/connectome-model-assignment/page.tsx
+++ src/app/build/(main)/connectome-model-assignment/page.tsx
@@ -1,14 +1,73 @@
 'use client';
 
+import { Suspense } from 'react';
+import { ConfigProvider, theme } from 'antd';
 import { useAtomValue } from 'jotai';
 import { initialRulesAtom } from './state';
+import { SynapticRule } from './types';
+import RightHemisphere from '@/components/right-hemisphere';
+import LeftHemisphere from '@/components/left-hemisphere';
 
-export default function ConnectomeModelAssignmentView() {
+function SynapticRuleItem({ ruleDef }: { ruleDef: SynapticRule }) {
+  const from = ruleDef.fromSClass || ruleDef.fromMType || '-';
+  const to = ruleDef.toSClass || ruleDef.toMType || '-';
+
+  return (
+    <div className="text-white flex justify-between">
+      <div className="inline-flex justify-between" style={{ minWidth: 200 }}>
+        <div className="inline-block" style={{ minWidth: 100 }}>
+          {from}
+        </div>
+        {ruleDef.fromRegion !== null && <span>{ruleDef.toRegion}</span>}
+        {ruleDef.fromHemisphere === 'left' && <LeftHemisphere />}
+        {ruleDef.fromHemisphere === 'right' && <RightHemisphere />}
+      </div>
+      <span>➜</span>
+      <div className="inline-flex justify-between" style={{ minWidth: 200 }}>
+        <div className="inline-block" style={{ minWidth: 100 }}>
+          {to}
+        </div>
+        {ruleDef.toRegion !== null && <span>{ruleDef.toRegion}</span>}
+        {ruleDef.toHemisphere === 'left' && <LeftHemisphere />}
+        {ruleDef.toHemisphere === 'right' && <RightHemisphere />}
+      </div>
+    </div>
+  );
+}
+
+function ConnectomeModelAssignmentView() {
   const initialRules = useAtomValue(initialRulesAtom);
 
   return (
-    <div className="p-4">
-      <h3>Connectome model assignment</h3>
+    <div className="p-4 bg-black h-full overflow-scroll">
+      <h1 className="text-white text-xl p-4 font-bold">Synapse Assignment Rules</h1>
+      <div className="mt-4 w-1/2">
+        {initialRules?.map((r, i) => (
+          <SynapticRuleItem ruleDef={r} key={i} />
+        ))}
+      </div>
     </div>
+  );
+}
+
+export default function ConnectomeModelAssignment() {
+  return (
+    <ConfigProvider
+      theme={{
+        algorithm: theme.darkAlgorithm,
+        components: {
+          Select: {
+            borderRadius: 0,
+          },
+          Tabs: {
+            colorPrimary: 'white',
+          },
+        },
+      }}
+    >
+      <Suspense fallback={null}>
+        <ConnectomeModelAssignmentView />
+      </Suspense>
+    </ConfigProvider>
   );
 }
