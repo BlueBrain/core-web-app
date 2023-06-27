0aa73cd9c7326854edad84ca60edfa6034d08ed3
Stefano Antonel <stefanoantonel@gmail.com>
2023-06-27 15:16:05 +0200

    Add Error boundary M-Model

--- src/app/build/(main)/cell-model-assignment/m-model/configuration/page.tsx
+++ src/app/build/(main)/cell-model-assignment/m-model/configuration/page.tsx
@@ -1,13 +1,33 @@
+'use client';
+
+import { ErrorBoundary } from 'react-error-boundary';
+import { useAtomValue } from 'jotai';
+
+import SimpleErrorComponent from '@/components/GenericErrorFallback';
 import {
   MModelPreviewImages,
   ParameterSliders,
 } from '@/components/build-section/cell-model-assignment/';
+import { selectedMModelNameAtom } from '@/state/brain-model-config/cell-model-assignment';
+import { selectedBrainRegionAtom } from '@/state/brain-regions';
 
 export default function ConfigurationPage() {
+  const mModelSelectedName = useAtomValue(selectedMModelNameAtom);
+  const selectedRegion = useAtomValue(selectedBrainRegionAtom);
+
+  if (!mModelSelectedName || !selectedRegion)
+    return (
+      <div className="flex h-screen items-center justify-center text-4xl">
+        Select region and M-Type
+      </div>
+    );
+
   return (
     <div className="m-5 flex">
-      <ParameterSliders />
-      <MModelPreviewImages className="ml-5 flex-grow flex" />
+      <ErrorBoundary FallbackComponent={SimpleErrorComponent}>
+        <ParameterSliders />
+        <MModelPreviewImages className="ml-5 flex-grow flex h-screen overflow-scroll" />
+      </ErrorBoundary>
     </div>
   );
 }
