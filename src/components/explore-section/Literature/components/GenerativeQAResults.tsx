1782c68fd9ab7987cdd7de3bcc136cc3a80bae98
Bilal MEDDAH <b_meddah@esi.dz>
2023-08-03 14:48:31 +0200

    134-632/update: move subcomponent to folder

---
+++ src/components/explore-section/Literature/components/GenerativeQAResults.tsx
@@ -0,0 +1,155 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import { FilterOutlined } from '@ant-design/icons';
+import trim from 'lodash/trim';
+import { useAtomValue } from 'jotai';
+import { format } from 'date-fns';
+import { literatureResultAtom, useLiteratureAtom } from '../state';
+import { scrollToBottom } from '../utils';
+import { TGenerativeQA } from '../types';
+import ArticlesTimeLine from './ArticlesTimeLine';
+import BrainLight from '@/components/icons/BrainLight';
+import { ChevronIcon } from '@/components/icons';
+
+export type TGenerativeQASingleResultProps = Omit<TGenerativeQA, 'sources' | 'paragraphs'>;
+function GenerativeQASingleResult({
+    id,
+    question,
+    askedAt,
+    answer,
+    rawAnswer,
+    articles,
+}: TGenerativeQASingleResultProps) {
+    const [expandArticles, setExpandArticles] = useState(false);
+    const [collpaseQuestion, setCollpaseQuestion] = useState(false);
+    const update = useLiteratureAtom();
+    const answerRef = React.useRef<HTMLDivElement>(null);
+    const toggleExpandArticles = () => setExpandArticles((state) => !state);
+    const toggleCollapseQuestion = () => setCollpaseQuestion((state) => !state);
+    const openFilterPanel = () => update('isFilterPanelOpen', true);
+
+    const showExtraDetails = Boolean(articles.length);
+    let finalAnswer = answer;
+
+    if (answer && trim(answer).length > 0) {
+        finalAnswer = answer;
+    } else if (rawAnswer && trim(rawAnswer).length > 0) {
+        finalAnswer = rawAnswer;
+    }
+
+    useEffect(() => {
+        if (answerRef.current && expandArticles) {
+            answerRef.current.scrollIntoView({
+                block: 'start',
+                behavior: 'smooth',
+            });
+        }
+    }, [expandArticles]);
+
+    return (
+        <div id={id} className={`w-full mt-3 ${collpaseQuestion ? 'mb-6' : 'mb-28'}`} ref={answerRef}>
+            <div className="inline-flex items-center w-full gap-2">
+                <div className="w-auto h-px bg-neutral-3 flex-[1_1]" />
+                <span className="pl-2 text-sm w-max text-neutral-4">
+                   Asked {format(new Date(askedAt), 'dd/MM/yyyy - kk:mm')}
+                </span>
+            </div>
+            <div className='inline-flex items-center justify-between w-full'>
+                <div className="inline-flex items-center justify-start w-full gap-2 my-5">
+                    <BrainLight />
+                    <span className={`font-normal tracking-tight text-blue-900 ${collpaseQuestion ? 'text-xl font-extrabold' : 'text-sm'}`}>
+                        {question}
+                    </span>
+                </div>
+                <button
+                    type="button"
+                    onClick={toggleCollapseQuestion}
+                    className='flex items-center justify-center w-8 h-8 p-px rounded-full hover:shadow-md'
+                >
+                    <ChevronIcon fill="#003A8C" className={`${collpaseQuestion ? 'rotate-0' : 'rotate-90'}`} />
+                </button>
+            </div>
+            <div className={`${collpaseQuestion ? 'hidden' : 'block'}`}>
+                <div className="w-full text-xl font-normal leading-7 text-blue-900">{finalAnswer}</div>
+                <div className={`w-full h-px my-6 bg-zinc-300 ${showExtraDetails ? '' : 'hidden'}`} />
+                <div className={`w-full ${showExtraDetails ? '' : 'hidden'}`}>
+                    <div className="inline-flex items-center justify-between w-full gap-y-3">
+                        <button
+                            type="button"
+                            onClick={toggleExpandArticles}
+                            className="w-[350px] h-11 px-3 py-3 gap-1.5 bg-blue-50 text-white shadow-sm  rounded-md relative inline-flex justify-between items-center whitespace-nowrap cursor-pointer no-underline leading-tight transition-all duration-200 "
+                            aria-controls="collapse-content"
+                        >
+                            <div className="inline-flex items-center justify-start gap-1">
+                                <div className="text-base font-normal leading-snug text-blue-900">Based on</div>
+                                <div className="px-1 py-[.2px] bg-blue-900 rounded-[3px] flex-col justify-start items-center inline-flex">
+                                    <span className="text-sm font-bold tracking-tight text-blue-50">
+                                        {articles.length}
+                                    </span>
+                                </div>
+                                <div className="text-base font-normal leading-snug text-blue-900">articles</div>
+                            </div>
+                            <ChevronIcon fill="#003A8C" className={`${expandArticles ? '-rotate-90' : 'rotate-90'}`} />
+                        </button>
+                        <button
+                            type="button"
+                            className="flex items-center gap-2 px-2 py-1 cursor-pointer"
+                            onClick={openFilterPanel}
+                        >
+                            <span className="text-base text-primary-8">Filter</span>
+                            <span className="px-2 py-1 bg-gray-200 rounded-sm hover:bg-primary-8 group">
+                                <FilterOutlined className="text-primary-8 group-hover:text-white" />
+                            </span>
+                        </button>
+                    </div>
+                    <div
+                        id="collapse-content"
+                        className={`mt-5 ${expandArticles
+                                ? 'block px-1 opacity-100 transition-all ease-out-expo duration-300 overflow-hidden'
+                                : 'hidden transition-all duration-100 ease-in-expo'
+                            }`}
+                        data-collapse-animate="on"
+                    >
+                        <div className="mt-4 rounded">
+                            <ArticlesTimeLine
+                                {...{
+                                    articles,
+                                    collapseAll: expandArticles,
+                                }}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function GenerativeQAResultList() {
+    const QAs = useAtomValue(literatureResultAtom);
+    useEffect(() => {
+        if (QAs.length > 0) {
+            scrollToBottom();
+        }
+    }, [QAs.length]);
+    return (
+        <div className="w-full max-w-4xl mx-auto mb-36">
+            {QAs.map(({ id, question, answer, rawAnswer, articles, askedAt }) => (
+                <GenerativeQASingleResult
+                    key={id}
+                    {...{
+                        id,
+                        askedAt,
+                        question,
+                        answer,
+                        rawAnswer,
+                        articles,
+                    }}
+                />
+            ))}
+        </div>
+    );
+}
+
+export default GenerativeQAResultList;
---
+++ src/components/explore-section/Literature/components/LiteratureSideBar.tsx
@@ -0,0 +1,119 @@
+'use client';
+
+import { useState } from 'react';
+import Link from 'next/link';
+import { Button } from 'antd';
+import {
+  ArrowRightOutlined,
+  HomeOutlined,
+  MinusOutlined,
+  PlusOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import SectionCards from '@/components/explore-section/SectionCards';
+import { sectionContent } from '@/constants/homes-sections/homeSectionContent';
+import type { TypeSingleCard } from '@/constants/homes-sections/homeSectionContent';
+
+function LiteratureSidebar() {
+  const [expanded, setExpanded] = useState(false);
+  const onExpand = () => setExpanded((prev) => !prev);
+
+  return (
+    <div
+      className={`fixed top-0 z-50 py-4 inline-flex flex-col items-center justify-start h-screen ${
+        expanded ? 'w-80' : 'w-10'
+      } transition-transform ease-in-out will-change-auto bg-primary-9 text-light`}
+    >
+      <div
+        className={`inline-flex w-full ${
+          expanded
+            ? 'flex-row-reverse items-center justify-between px-4 mb-4'
+            : ' flex-col items-center justify-center gap-6 mb-auto'
+        }`}
+      >
+        <Button
+          type="text"
+          className="relative order-1"
+          onClick={onExpand}
+          style={{ color: 'white' }}
+          icon={expanded ? <MinusOutlined /> : <PlusOutlined />}
+        />
+        <span
+          className={`relative order-2 p-0 text-white ${
+            expanded ? 'font-bold text-2xl' : '-rotate-90 font-semibold'
+          }`}
+        >
+          Literature
+        </span>
+      </div>
+      {expanded && (
+        <div className="flex flex-col w-full h-full px-4 overflow-y-auto primary-scrollbar gap-y-3">
+          {sectionContent.map((singleCard: TypeSingleCard, index: number) => (
+            <SectionCards
+              content={singleCard}
+              cardIndex={index}
+              expanded={expanded}
+              key={`explore-section-card-${singleCard.name}`}
+            />
+          ))}
+        </div>
+      )}
+      <div className="relative bottom-0 w-full mt-auto h-max">
+        <ul
+          className={`w-full ${
+            expanded
+              ? 'px-4 py-4'
+              : 'flex flex-col items-center justify-center gap-2 absolute bottom-4'
+          }`}
+        >
+          <li
+            className={`${
+              expanded
+                ? 'border-b border-primary-7 w-full flex items-center justify-between py-2'
+                : ''
+            }`}
+          >
+            <Link
+              href="/"
+              className={`${
+                expanded ? 'font-semibold w-full flex items-center justify-between' : ''
+              }`}
+            >
+              {expanded && 'Home'}
+              <HomeOutlined />
+            </Link>
+          </li>
+          <li className={`${expanded ? 'w-full flex items-center justify-between py-2' : ''} `}>
+            <Link
+              href="/"
+              className={`${
+                expanded ? 'font-semibold w-full flex items-center justify-between' : ''
+              }`}
+            >
+              {expanded && 'User'}
+              <UserOutlined />
+            </Link>
+          </li>
+        </ul>
+        {expanded && (
+          <ul className="flex flex-col w-full p-4 gap-y-2 h-max">
+            <li className="p-2 text-white bg-primary-6">
+              <Link href="/build" className="flex items-center justify-between gap-2 ">
+                <h2>Build</h2>
+                <ArrowRightOutlined />
+              </Link>
+            </li>
+            <li className="p-2 text-white bg-primary-6">
+              <Link href="/simulate" className="flex items-center justify-between gap-2">
+                <h2>Simulate</h2>
+                <ArrowRightOutlined />
+              </Link>
+            </li>
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default LiteratureSidebar;
