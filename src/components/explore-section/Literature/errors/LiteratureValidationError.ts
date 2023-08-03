c124d656eaec0aa5f0023efd46ef8364d68caa10
Bilal MEDDAH <b_meddah@esi.dz>
2023-08-03 14:48:54 +0200

    134-632/update: fix errors class

--- src/components/explore-section/Literature/errors/LiteratureValidationError.ts
+++ src/components/explore-section/Literature/errors/LiteratureValidationError.ts
@@ -1,18 +1,17 @@
 type TCostumError = {
-    loc: string[];
-    msg: string;
-    type: string;
-}
-
+  loc: string[];
+  msg: string;
+  type: string;
+};
 
 class LiteratureValidationError extends Error {
-    detail: TCostumError;
+  detail: TCostumError;
 
-    constructor(detail: TCostumError) {
-        super(detail.msg, { cause: detail });
-        this.name = 'LiteratureValidationError';
-        this.detail = detail;
-    }
+  constructor(detail: TCostumError) {
+    super(detail.msg, { cause: detail });
+    this.name = 'LiteratureValidationError';
+    this.detail = detail;
+  }
 }
 
 export default LiteratureValidationError;
