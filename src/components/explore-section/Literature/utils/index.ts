211d6539c835b30b243341c39353a86de229c042
Bilal MEDDAH <b_meddah@esi.dz>
2023-08-07 15:08:13 +0200

    134-632/update: fn already exists

--- src/components/explore-section/Literature/utils/index.ts
+++ src/components/explore-section/Literature/utils/index.ts
@@ -1,4 +1,3 @@
 export { default as highlightHits } from './highlightHits';
-export { default as formatNumber } from './formatNumber';
 export { default as copyClipboard } from './copyClipboard';
 export { default as scrollToBottom } from './scrollToBottom';
