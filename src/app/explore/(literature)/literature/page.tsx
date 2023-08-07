a3aaf2372dc3e9cda471ebaf878c319659668a00
Bilal MEDDAH <b_meddah@esi.dz>
2023-08-07 13:00:11 +0200

    134-662/update: fix styles

--- src/app/explore/(literature)/literature/page.tsx
+++ src/app/explore/(literature)/literature/page.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
-import {
-    GenerativeQAContainer
-} from '@/components/explore-section/Literature/components';
+import { GenerativeQAContainer } from '@/components/explore-section/Literature/components';
 import SimpleErrorComponent from '@/components/GenericErrorFallback';
 
 export default async function LiteraturePage() {
-    return (
-        <ErrorBoundary FallbackComponent={SimpleErrorComponent}>
-            <GenerativeQAContainer />
-        </ErrorBoundary>
-    );
+  return (
+    <ErrorBoundary FallbackComponent={SimpleErrorComponent}>
+      <GenerativeQAContainer />
+    </ErrorBoundary>
+  );
 }
