5f00d4e7d5bd9d254462bc986469681db328229e
Bilal MEDDAH <b_meddah@esi.dz>
2023-08-08 21:23:14 +0200

    134-632/update: remove redirect in se due server action failing to redirect

--- src/components/explore-section/Literature/api.ts
+++ src/components/explore-section/Literature/api.ts
@@ -1,23 +1,15 @@
-import { getServerSession } from 'next-auth';
-import { redirect } from 'next/navigation';
 import { GenerativeQAResponse, ReturnGetGenerativeQA } from './types';
 import { generativeQADTO } from './utils/DTOs';
 import * as LiteratureErrors from './errors';
-import { authOptions } from '@/pages/api/auth/[...nextauth]';
 import { nexus } from '@/config';
 
 const getGenerativeQA: ReturnGetGenerativeQA = async ({ question }) => {
-  const session = await getServerSession(authOptions);
-  if (!session) {
-    redirect('/api/auth/signin');
-  }
   try {
     const response = await fetch(`${nexus.aiUrl}/generative_qa`, {
       method: 'POST',
       headers: {
         accept: 'application/json',
         'Content-Type': 'application/json',
-        Authorization: `Bearer ${session.accessToken}`,
       },
       body: JSON.stringify({
         query: question,
