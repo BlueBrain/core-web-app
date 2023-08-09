2a0f67272f83c03765e6e1d6f5f0dc6ec6860964
Bilal MEDDAH <b_meddah@esi.dz>
2023-08-09 14:21:22 +0200

    134-662/update: styles of qa input and results list

--- src/components/explore-section/Literature/components/GenerativeQAResults.tsx
+++ src/components/explore-section/Literature/components/GenerativeQAResults.tsx
@@ -1,12 +1,11 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { FilterOutlined } from '@ant-design/icons';
-import trim from 'lodash/trim';
 import { useAtomValue } from 'jotai';
 import { format } from 'date-fns';
+import trim from 'lodash/trim';
 
-import { scrollToBottom } from '../utils';
 import { GenerativeQA } from '../types';
 import ArticlesTimeLine from './ArticlesTimeLine';
 import BrainLight from '@/components/icons/BrainLight';
@@ -144,30 +143,40 @@ function GenerativeQASingleResult({
   );
 }
 
-function GenerativeQAResultList() {
+function QAResultList() {
+  const qaResultsRef = useRef<HTMLDivElement>(null);
   const QAs = useAtomValue(literatureResultAtom);
+
   useEffect(() => {
-    if (QAs.length > 0) {
-      scrollToBottom();
+    if (qaResultsRef.current) {
+      qaResultsRef.current.scrollTo({
+        behavior: 'smooth',
+        top: qaResultsRef.current.scrollHeight,
+      });
     }
   }, [QAs.length]);
+
   return (
-    <div className="w-full max-w-4xl mx-auto mb-36">
-      {QAs.map(({ id, question, answer, rawAnswer, articles, askedAt }) => (
-        <GenerativeQASingleResult
-          key={id}
-          {...{
-            id,
-            askedAt,
-            question,
-            answer,
-            rawAnswer,
-            articles,
-          }}
-        />
-      ))}
+    <div className="w-full h-full max-h-screen">
+      <div className="flex-1 w-full h-full overflow-auto scroll-smooth" ref={qaResultsRef}>
+        <ul className="flex flex-col items-center justify-start max-w-4xl p-4 mx-auto list-none">
+          {QAs.map(({ id, question, answer, rawAnswer, articles, askedAt }) => (
+            <GenerativeQASingleResult
+              key={id}
+              {...{
+                id,
+                askedAt,
+                question,
+                answer,
+                rawAnswer,
+                articles,
+              }}
+            />
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
 
-export default GenerativeQAResultList;
+export default QAResultList;
