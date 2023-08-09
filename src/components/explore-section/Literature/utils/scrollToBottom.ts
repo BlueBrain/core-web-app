a7762605dfe10fba2671c548502f3b0f671487d6
Bilal MEDDAH <b_meddah@esi.dz>
2023-08-09 14:18:50 +0200

    134-662/update: remove unnecessary component and utility fn

--- src/components/explore-section/Literature/utils/scrollToBottom.ts
+++
@@ -1,9 +0,0 @@
-export default function scrollToBottom(timeout: number = 200) {
-  const timeoutId = setTimeout(() => {
-    window.scrollTo({
-      behavior: 'smooth',
-      top: document.body.scrollHeight,
-    });
-    clearTimeout(timeoutId);
-  }, timeout);
-}
