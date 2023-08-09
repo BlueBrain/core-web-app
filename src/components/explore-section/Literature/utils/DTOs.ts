3b798d5ff6d784087f07a233b26dad7e724f58b4
Bilal MEDDAH <b_meddah@esi.dz>
2023-08-09 14:21:48 +0200

    134-662/update: id generation for questions and articles

--- src/components/explore-section/Literature/utils/DTOs.ts
+++ src/components/explore-section/Literature/utils/DTOs.ts
@@ -8,13 +8,15 @@ type GenerativeQADTO = {
 
 const generativeQADTO = ({ question, generativeQAResponse }: GenerativeQADTO): GenerativeQA => {
   const askedAt = new Date();
+  const questionId = `${kebabCase(question)}-${askedAt.getTime()}`;
   return {
     question,
-    id: `${kebabCase(question)}-${askedAt.getTime()}}`,
+    id: questionId,
     askedAt: new Date(),
     answer: generativeQAResponse.answer,
     rawAnswer: generativeQAResponse.raw_answer.split('SOURCES')?.[0],
-    articles: generativeQAResponse.metadata.map((article) => ({
+    articles: generativeQAResponse.metadata.map((article, index) => ({
+      key: `${questionId}-${article.article_id}-${index}`,
       id: article.article_id,
       doi: article.article_doi,
       title: article.article_title,
