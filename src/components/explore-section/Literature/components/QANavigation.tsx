63c4f94817e0e2b46d42949d4a41f24931333828
Bilal Meddah <bilal.meddah@epfl.ch>
2023-08-18 14:39:40 +0000

    Update: add brain region if exist for question in questions navigation

--- src/components/explore-section/Literature/components/QANavigation.tsx
+++ src/components/explore-section/Literature/components/QANavigation.tsx
@@ -6,6 +6,7 @@ import { DeleteOutlined } from '@ant-design/icons';
 import delay from 'lodash/delay';
 import last from 'lodash/last';
 
+import { BrainIcon } from '@/components/icons';
 import { classNames } from '@/util/utils';
 import {
   brainRegionQAs,
@@ -17,11 +18,19 @@ import {
 import { GenerativeQA } from '@/types/literature';
 import usePathname from '@/hooks/pathname';
 
-type QAHistoryNavigationItemProps = Pick<GenerativeQA, 'id' | 'question' | 'askedAt'> & {
+type QAHistoryNavigationItemProps = Pick<
+  GenerativeQA,
+  'id' | 'question' | 'askedAt' | 'brainRegion'
+> & {
   index: number;
 };
 
-function QAHistoryNavigationItem({ id, index, question }: QAHistoryNavigationItemProps) {
+function QAHistoryNavigationItem({
+  id,
+  index,
+  question,
+  brainRegion,
+}: QAHistoryNavigationItemProps) {
   const [isDeleting, setIsDeleting] = useState(false);
   const { activeQuestionId } = useAtomValue(literatureAtom);
   const { remove } = useLiteratureResultsAtom();
@@ -45,30 +54,40 @@ function QAHistoryNavigationItem({ id, index, question }: QAHistoryNavigationIte
       role="button"
       onClick={onClick}
       className={classNames(
-        'relative inline-flex items-center w-full pl-16 mb-2 list-none gqa-nav-item text-neutral-8 ',
+        'relative inline-flex items-center w-full pl-16 py-4 pr-2 mb-2 list-none gqa-nav-item text-neutral-8 hover:bg-gray-50 rounded-r-sm group',
         isDeleting ? 'bg-gray-100 overflow-hidden py-4 animate-slide-out' : ''
       )}
     >
       {isActive && (
         <div
           className={classNames(
-            'absolute inline-flex w-10 h-[3.5px] transition-all duration-200 ease-in-expo transform rounded-full top-[10.5px] left-3 bg-primary-8',
+            'absolute inline-flex w-10 h-[3.5px] transition-all duration-200 ease-in-expo transform rounded-full top-6 left-3 bg-primary-8',
             isDeleting ? 'hidden' : ''
           )}
         />
       )}
       <div className="flex flex-col items-start justify-start w-full">
         <div className="inline-flex items-center justify-between w-full gap-2">
-          <div className="text-sm font-medium capitalize text-neutral-3">question {index}</div>
+          <div className="text-sm font-medium capitalize text-neutral-3 group-hover:text-neutral-4">
+            question {index}
+          </div>
           <DeleteOutlined
-            className="text-sm transition-all transform scale-110 text-neutral-3 hover:text-primary-8"
+            className="text-sm hover:transition-all hover:transform hover:scale-110 text-neutral-3 hover:text-primary-8 group-hover:text-primary-8"
             onClick={onDelete}
           />
         </div>
+        {brainRegion?.id && (
+          <div className="inline-flex items-center gap-2 my-1">
+            <BrainIcon className="text-primary-5" />
+            <span title={brainRegion.title} className="text-base text-primary-5 line-clamp-1">
+              {brainRegion.title}
+            </span>
+          </div>
+        )}
         <div
           title={question}
           className={classNames(
-            'w-4/5 text-xl line-clamp-2 ',
+            'w-4/5 text-xl line-clamp-2 group-hover:text-primary-8 group-hover:text-primary-8',
             isActive ? 'text-primary-8 font-bold' : 'text-neutral-3 font-medium'
           )}
           data-testid="question-navigation-item"
@@ -127,6 +146,7 @@ function QAHistoryNavigation() {
             askedAt: qa.askedAt,
             question: qa.question,
             articles: qa.articles,
+            brainRegion: qa.brainRegion,
           }}
         />
       ))}
