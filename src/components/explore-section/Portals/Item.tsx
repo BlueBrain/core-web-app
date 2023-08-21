3ddbada9801edb26639254da63b8bde03316b8a0
Loris Olivier <53363974+loris-maru@users.noreply.github.com>
2023-08-21 10:24:58 +0200

    Change useState setter and value to isMouseOver and setIsMouseOver

--- src/components/explore-section/Portals/Item.tsx
+++ src/components/explore-section/Portals/Item.tsx
@@ -9,19 +9,19 @@ import { ArrowRightIcon } from '@/components/icons';
 import { classNames } from '@/util/utils';
 
 export default function PortalItem({ content }: { content: Portals }) {
-  const [mouseStatus, setMouseStatus] = useState<boolean>(false);
+  const [isMouseOver, setIsMouseOver] = useState<boolean>(false);
 
   return (
     <Link
       href={content.url}
       className={classNames(
         'relative w-full h-48 p-3 flex flex-row items-center justify-between rounded transition-background duration-300 ease-linear',
-        mouseStatus ? 'bg-primary-7' : 'bg-primary-8'
+        isMouseOver ? 'bg-primary-7' : 'bg-primary-8'
       )}
-      onMouseOver={() => setMouseStatus(true)}
-      onFocus={() => setMouseStatus(true)}
-      onMouseOut={() => setMouseStatus(false)}
-      onBlur={() => setMouseStatus(false)}
+      onMouseOver={() => setIsMouseOver(true)}
+      onFocus={() => setIsMouseOver(true)}
+      onMouseOut={() => setIsMouseOver(false)}
+      onBlur={() => setIsMouseOver(false)}
     >
       <div className="w-90percent h-full flex flex-row items-center">
         {/* Image */}
@@ -42,7 +42,7 @@ export default function PortalItem({ content }: { content: Portals }) {
         <ArrowRightIcon
           className={classNames(
             'absolute text-white w-auto h-3 transition-right duration-500 ease-in-out',
-            mouseStatus ? 'right-0' : '-right-8'
+            isMouseOver ? 'right-0' : '-right-8'
           )}
         />
       </div>
