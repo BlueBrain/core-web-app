ad56aca83d4134e6d9969c1fa2e764b5e86b4121
Pavlo Getta <pavlo.getta@epfl.ch>
2023-09-01 13:20:33 +0000

    Fix sign-in redirect base path

--- src/hooks/auth.ts
+++ src/hooks/auth.ts
@@ -2,28 +2,6 @@
 
 import { useEffect } from 'react';
 import { signIn, signOut, useSession } from 'next-auth/react';
-import { usePathname, useSearchParams } from 'next/navigation';
-
-/**
- * Create an absolute URL (string) of the current page using pathname and searchParams
- * from `next/navigation` component so that it's consistent with Next.js routing state.
- * This is used for sign-in to always redirect the user to the current page.
- *
- * See a comment below for more details about the issue.
- */
-function useCallbackUrl() {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-
-  if (!pathname || typeof window === 'undefined') return undefined;
-
-  const url = new URL(window.location.href);
-
-  url.pathname = pathname;
-  url.search = searchParams?.toString() ?? '';
-
-  return url.toString();
-}
 
 /**
  * Provides necessary logic to handle auth session:
@@ -34,19 +12,11 @@ function useCallbackUrl() {
  * @param {boolean} required - force signIn if user isn't authenticated.
  */
 export default function useAuth(required = false) {
-  /*
-    Window.location used by next-auth to create default callbackUrl points to a previous
-    location while route change is pending, thus sign-in in some cases returns the user to a previous page.
-
-    To ovecome that using next/navigation to create a callbackUrl.
-  */
-  const callbackUrl = useCallbackUrl();
-
   const { data: session } = useSession({
     required,
     onUnauthenticated() {
       // this is executed only if `required` is set to true
-      signIn('keycloak', { callbackUrl });
+      signIn('keycloak');
     },
   });
 
@@ -55,9 +25,9 @@ export default function useAuth(required = false) {
 
     // automatically signIn/signOut when a refresh token expires
     if (required) {
-      signIn('keycloak', { callbackUrl });
+      signIn('keycloak');
     } else {
       signOut();
     }
-  }, [session, callbackUrl, required]);
+  }, [session, required]);
 }
