dd137c8e126a46dc691fb30b14fc7eb80d20be0b
Pavlo Getta <pavlo.getta@epfl.ch>
2023-10-09 07:32:33 +0000

    E-model interactive view: add missing Suspense boundary

--- src/app/build/(main)/cell-model-assignment/e-model/interactive/page.tsx
+++ src/app/build/(main)/cell-model-assignment/e-model/interactive/page.tsx
@@ -1,10 +1,16 @@
 'use client';
 
+import { Suspense } from 'react';
+
 import InteractiveView from '@/components/build-section/cell-model-assignment/e-model/interactive/View';
 import useLiteratureCleanNavigate from '@/components/explore-section/Literature/useLiteratureCleanNavigate';
 
 export default function InteractivePage() {
   useLiteratureCleanNavigate();
 
-  return <InteractiveView />;
+  return (
+    <Suspense fallback={null}>
+      <InteractiveView />
+    </Suspense>
+  );
 }
