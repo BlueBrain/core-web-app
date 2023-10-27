b907f6fac618481b28f0d706f62c1648201eca16
Fabien Petitjean <fabien.petitjean@epfl.ch>
2023-10-27 14:56:25 +0000

    Add whole mouse brain to the list of meshes to always display.

--- src/components/experiment-designer/experiment-setup/Visualization/index.tsx
+++ src/components/experiment-designer/experiment-setup/Visualization/index.tsx
@@ -1,18 +1,32 @@
-import { useMemo } from 'react';
-import { useAtomValue } from 'jotai';
+import { useAtomValue, useSetAtom } from 'jotai';
 import isArray from 'lodash/isArray';
-import { ExpDesignerSectionName } from '@/types/experiment-designer';
+import { useEffect, useMemo } from 'react';
+
 import SimulationPreview from '@/components/experiment-designer/simulation-preview';
 import {
   extractTargetNamesFromSection,
   getFocusedAtom,
 } from '@/components/experiment-designer/utils';
+import { atlasVisualizationAtom, initializeRootMeshAtom } from '@/state/atlas/atlas';
+import { ExpDesignerSectionName } from '@/types/experiment-designer';
 
 type VisualizationProps = {
   sectionName: ExpDesignerSectionName;
 };
 
 export default function Visualization({ sectionName }: VisualizationProps) {
+  const atlasVisualization = useAtomValue(atlasVisualizationAtom);
+  const initRootMesh = useSetAtom(initializeRootMeshAtom);
+  useEffect(() => {
+    /**
+     * We display all the meshes that have been selected in the brain's regions' tree.
+     * But if none has been selected yet (most of the time because we don't come from
+     * that path), then we need to display at least the whole brain.
+     */
+    if (atlasVisualization.visibleMeshes.length > 0) return;
+
+    initRootMesh();
+  }, [atlasVisualization, initRootMesh]);
   const focusedAtom = useMemo(() => {
     const name: ExpDesignerSectionName = sectionName === 'imaging' ? 'setup' : sectionName;
     return getFocusedAtom(name);
