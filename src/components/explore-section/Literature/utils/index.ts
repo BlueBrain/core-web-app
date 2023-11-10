b7fa372a4ecad811b1446eaf1f26dfa1344ba80a
Bilal Meddah <bilal.meddah@epfl.ch>
2023-11-10 10:07:04 +0100

    134-1061/update: add new utility fn to update nested object and merge it again to origin array

--- src/components/explore-section/Literature/utils/index.ts
+++ src/components/explore-section/Literature/utils/index.ts
@@ -1,2 +1,15 @@
+import find from 'lodash/find';
+import merge from 'lodash/merge';
+
 export { default as highlightHits } from './highlightHits';
 export { default as copyClipboard } from './copyClipboard';
+
+export const updateAndMerge = <T>(
+  array: Array<T>,
+  keyfn: (item: T) => boolean,
+  newVal: any
+): Array<T> => {
+  const match = find(array, keyfn);
+  if (match) merge(match, newVal);
+  return array;
+};
