23f08b07e5a415a05a62bcc4e403c1dfd22586ee
Bilal Meddah <bilal.meddah@epfl.ch>
2023-11-10 16:25:22 +0100

    134-1075/update: rebuild home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import AuthWrapper from '@/components/AuthWrapper';
-import Home from '@/components/Home';
+import Entrypoint from '@/components/Entrypoint';
 
 export default function RootPage() {
   return (
     <AuthWrapper>
-      <Home />
+      <Entrypoint />
     </AuthWrapper>
   );
 }
