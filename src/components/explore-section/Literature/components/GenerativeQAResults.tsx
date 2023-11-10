fba864f7d0e514aeafc2bab6862ecdaa6a1d7daa
Bilal Meddah <bilal.meddah@epfl.ch>
2023-11-10 10:17:10 +0100

    134-1061/update: Result/Results components

--- src/components/explore-section/Literature/components/GenerativeQAResults.tsx
+++ src/components/explore-section/Literature/components/GenerativeQAResults.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useImperativeHandle, useReducer, useRef, useState } from 'react';
+import { useEffect, useReducer, useRef, useState, memo, ReactNode } from 'react';
 import { FilterOutlined } from '@ant-design/icons';
 import { useAtomValue } from 'jotai';
 import { format } from 'date-fns';
@@ -8,7 +8,8 @@ import { Button } from 'antd';
 import trim from 'lodash/trim';
 import delay from 'lodash/delay';
 
-import useContextualLiteratureContext from '../useContextualLiteratureContext';
+import useStreamGenerative from '../useStreamGenerative';
+import { useLiteratureDataSource } from '../useContextualLiteratureContext';
 import ArticlesTimeLine from './ArticlesTimeLine';
 import ArticleSorter from './ArticleSorter';
 import { QABrainRegionPerQuestion } from './QABrainRegion';
@@ -18,6 +19,7 @@ import {
   SortFn,
   SucceededGenerativeQA,
   FailedGenerativeQA,
+  SelectedBrainRegionPerQuestion,
 } from '@/types/literature';
 import { ChevronIcon } from '@/components/icons';
 import { classNames } from '@/util/utils';
@@ -53,7 +55,7 @@ function GenerativeQASingleResultContainer({
   id: string;
   className?: string;
   moreSpace: boolean;
-  children: React.ReactNode;
+  children: ReactNode;
 }) {
   return (
     <div
@@ -304,7 +306,7 @@ function GenerativeQASingleResult({
     ? [...filteredArticles].sort(sortFunction)
     : filteredArticles;
 
-  const showExtraDetails = Boolean(articles.length);
+  const showExtraDetails = Boolean(articles?.length);
 
   let finalAnswer = answer;
   if (answer && trim(answer).length > 0) {
@@ -347,103 +349,143 @@ function GenerativeQASingleResult({
         <div className={`${collpaseQuestion ? 'hidden' : 'block'}`}>
           <div className="w-full text-xl font-normal leading-7 text-blue-900">{finalAnswer}</div>
           <div className={`w-full h-px my-6 bg-zinc-300 ${showExtraDetails ? '' : 'hidden'}`} />
-          <div className={`w-full ${showExtraDetails ? '' : 'hidden'}`}>
-            <div className="inline-flex items-center justify-between w-full gap-y-3">
-              <div className="flex items-center w-full">
+          {articles && Boolean(articles.length) && (
+            <div className={`w-full ${showExtraDetails ? '' : 'hidden'}`}>
+              <div className="inline-flex items-center justify-between w-full gap-y-3">
+                <div className="flex items-center w-full">
+                  <button
+                    type="button"
+                    onClick={toggleExpandArticles}
+                    className="h-11 w-[300px] px-3 py-3 gap-1.5 bg-blue-50 text-white shadow-sm  rounded-md relative inline-flex justify-between items-center whitespace-nowrap cursor-pointer no-underline leading-tight transition-all duration-200"
+                    aria-controls="collapse-content"
+                    aria-label="expand-articles"
+                  >
+                    <div className="inline-flex items-center justify-start gap-1">
+                      <div className="text-base font-normal leading-snug text-blue-900">
+                        Based on
+                      </div>
+                      <div className="px-1 py-[.2px] bg-blue-900 rounded-[3px] flex-col justify-start items-center inline-flex">
+                        <span className="text-sm font-bold tracking-tight text-blue-50">
+                          {articles.length}
+                        </span>
+                      </div>
+                      <div className="text-base font-normal leading-snug text-blue-900">
+                        paragraphs
+                      </div>
+                    </div>
+                    <ChevronIcon
+                      className={classNames(
+                        'text-primary-8 fill-current',
+                        expandArticles ? '-rotate-90' : 'rotate-90'
+                      )}
+                    />
+                  </button>
+                  <ArticleSorter
+                    onChange={(sortFn) => {
+                      setSortFunction(() => sortFn);
+                    }}
+                  />
+                </div>
                 <button
                   type="button"
-                  onClick={toggleExpandArticles}
-                  className="h-11 w-[300px] px-3 py-3 gap-1.5 bg-blue-50 text-white shadow-sm  rounded-md relative inline-flex justify-between items-center whitespace-nowrap cursor-pointer no-underline leading-tight transition-all duration-200"
-                  aria-controls="collapse-content"
-                  aria-label="expand-articles"
+                  className="flex items-center gap-2 px-2 py-1 cursor-pointer"
+                  onClick={selectQuestion}
                 >
-                  <div className="inline-flex items-center justify-start gap-1">
-                    <div className="text-base font-normal leading-snug text-blue-900">Based on</div>
-                    <div className="px-1 py-[.2px] bg-blue-900 rounded-[3px] flex-col justify-start items-center inline-flex">
-                      <span className="text-sm font-bold tracking-tight text-blue-50">
-                        {articles.length}
-                      </span>
-                    </div>
-                    <div className="text-base font-normal leading-snug text-blue-900">
-                      paragraphs
-                    </div>
-                  </div>
-                  <ChevronIcon
-                    className={classNames(
-                      'text-primary-8 fill-current',
-                      expandArticles ? '-rotate-90' : 'rotate-90'
-                    )}
-                  />
+                  <span className="text-base text-primary-8">Filter</span>
+                  <span className="px-2 py-1 bg-gray-200 rounded-sm hover:bg-primary-8 group">
+                    <FilterOutlined className="text-primary-8 group-hover:text-white" />
+                  </span>
                 </button>
-                <ArticleSorter
-                  onChange={(sortFn) => {
-                    setSortFunction(() => sortFn);
-                  }}
-                />
               </div>
-              <button
-                type="button"
-                className="flex items-center gap-2 px-2 py-1 cursor-pointer"
-                onClick={selectQuestion}
+              <div
+                id="collapse-content"
+                className={`mt-5 ${
+                  expandArticles
+                    ? 'block px-1 opacity-100 transition-all ease-out-expo duration-300 overflow-hidden'
+                    : 'hidden transition-all duration-100 ease-in-expo'
+                }`}
+                data-collapse-animate="on"
               >
-                <span className="text-base text-primary-8">Filter</span>
-                <span className="px-2 py-1 bg-gray-200 rounded-sm hover:bg-primary-8 group">
-                  <FilterOutlined className="text-primary-8 group-hover:text-white" />
-                </span>
-              </button>
-            </div>
-            <div
-              id="collapse-content"
-              className={`mt-5 ${
-                expandArticles
-                  ? 'block px-1 opacity-100 transition-all ease-out-expo duration-300 overflow-hidden'
-                  : 'hidden transition-all duration-100 ease-in-expo'
-              }`}
-              data-collapse-animate="on"
-            >
-              <div className="mt-4 rounded">
-                <ArticlesTimeLine
-                  {...{
-                    articles: displayedArticles,
-                    collapseAll: expandArticles,
-                  }}
-                />
+                <div className="mt-4 rounded">
+                  <ArticlesTimeLine
+                    {...{
+                      articles: displayedArticles,
+                      collapseAll: expandArticles,
+                    }}
+                  />
+                </div>
               </div>
             </div>
-          </div>
+          )}
         </div>
       </>
     </GenerativeQASingleResultContainer>
   );
 }
 
-function QAResultList({
-  imperativeRef,
-}: {
-  imperativeRef: React.RefObject<{ goToBottom: () => void }>;
-}) {
-  const qaResultsRef = useRef<HTMLDivElement>(null);
-  const firstRenderRef = useRef(false);
-  const { dataSource } = useContextualLiteratureContext();
+const GenerativeQAResult = memo(
+  ({
+    id,
+    streamed,
+    question,
+    askedAt,
+    brainRegion,
+    goToBottom,
+  }: {
+    id: string;
+    question: string;
+    askedAt: Date;
+    streamed: boolean;
+    brainRegion: SelectedBrainRegionPerQuestion | undefined;
+    goToBottom: () => void;
+  }) => {
+    const { answer, current } = useStreamGenerative({
+      id,
+      question,
+      streamed,
+      goToBottom,
+    });
 
-  useImperativeHandle(imperativeRef, () => ({
-    goToBottom() {
-      delay(() => {
-        if (qaResultsRef.current) {
-          qaResultsRef?.current.scrollTo({
-            behavior: 'smooth',
-            top: qaResultsRef.current.scrollHeight,
-          });
-        }
-      }, 500);
-    },
-  }));
+    return (
+      <GenerativeQASingleResult
+        key={id}
+        answer={answer}
+        rawAnswer={(current as SucceededGenerativeQA)?.rawAnswer ?? ''}
+        articles={(current as SucceededGenerativeQA)?.articles ?? []}
+        streamed={(current as SucceededGenerativeQA)?.streamed ?? true}
+        isNotFound={(current as SucceededGenerativeQA)?.isNotFound ?? false}
+        {...{
+          id,
+          askedAt,
+          question,
+          brainRegion,
+        }}
+      />
+    );
+  }
+);
+
+function QAResultList() {
+  const firstRenderRef = useRef(false);
+  const resultsContainerRef = useRef<HTMLDivElement>(null);
+  const dataSource = useLiteratureDataSource();
+
+  const goToBottom = () => {
+    delay(() => {
+      if (resultsContainerRef.current) {
+        resultsContainerRef?.current.scrollTo({
+          behavior: 'smooth',
+          top: resultsContainerRef.current.scrollHeight,
+        });
+      }
+    }, 500);
+  };
 
   useEffect(() => {
-    if (dataSource.length > 0 && qaResultsRef.current && !firstRenderRef.current) {
-      qaResultsRef?.current.scrollTo({
+    if (dataSource.length > 0 && resultsContainerRef.current && !firstRenderRef.current) {
+      resultsContainerRef?.current.scrollTo({
         behavior: 'smooth',
-        top: qaResultsRef.current.scrollHeight,
+        top: resultsContainerRef.current.scrollHeight,
       });
       firstRenderRef.current = true;
     }
@@ -451,43 +493,21 @@ function QAResultList({
 
   return (
     <div className="w-full h-full max-h-screen transition-height duration-700 ease-linear">
-      <div className="flex-1 w-full h-full overflow-auto scroll-smooth" ref={qaResultsRef}>
+      <div className="flex-1 w-full h-full overflow-auto scroll-smooth" ref={resultsContainerRef}>
         <ul className="flex flex-col items-center justify-start max-w-4xl p-4 mx-auto list-none">
-          {dataSource.map(({ id, question, askedAt, brainRegion, isNotFound, ...rest }) =>
-            isNotFound ? (
-              <GenerativeQASingleResultError
-                key={id}
-                statusCode={(rest as FailedGenerativeQA).statusCode}
-                details={(rest as FailedGenerativeQA).details}
-                {...{
-                  id,
-                  question,
-                  askedAt,
-                  brainRegion,
-                  isNotFound,
-                }}
-              />
-            ) : (
-              <GenerativeQASingleResult
-                key={id}
-                answer={(rest as SucceededGenerativeQA).answer}
-                rawAnswer={(rest as SucceededGenerativeQA).rawAnswer}
-                articles={(rest as SucceededGenerativeQA).articles}
-                {...{
-                  id,
-                  askedAt,
-                  question,
-                  brainRegion,
-                  isNotFound,
-                }}
-              />
-            )
-          )}
+          {dataSource.map(({ id, question, askedAt, streamed, brainRegion, chatId }) => (
+            <GenerativeQAResult
+              key={id}
+              goToBottom={goToBottom}
+              {...{ id, question, askedAt, streamed, brainRegion, chatId }}
+            />
+          ))}
         </ul>
       </div>
     </div>
   );
 }
 
+GenerativeQAResult.displayName = 'GenerativeQAResult';
 export { GenerativeQASingleResult, GenerativeQASingleResultError, GenerativeQASingleResultCompact };
 export default QAResultList;
