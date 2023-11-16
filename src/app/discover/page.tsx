aa84652ceecfc350687a8204ba41cd87cfe4e7df
Bilal Meddah <bilal.meddah@epfl.ch>
2023-11-16 10:47:51 +0100

    134-1097/update: add discover page

---
+++ src/app/discover/page.tsx
@@ -0,0 +1,86 @@
+'use client';
+
+import Link from 'next/link';
+
+import ApplicationSidebar, {
+  DefaultAccountPanel,
+  NavigationItem,
+  NavigationItemProps,
+} from '@/components/ApplicationSidebar';
+import { OBPLogo } from '@/components/Entrypoint/segments/Splash';
+import { EXPLORE_NAVIGATION_LIST } from '@/components/explore-section/Sidebar';
+import { classNames } from '@/util/utils';
+import { EyeIcon } from '@/components/icons';
+
+function MainSideBarHeader({ expanded }: { expanded: boolean }) {
+  return expanded ? <span>OBP</span> : <span>Menu</span>;
+}
+
+export function DiscoverNavigationItem({
+  url,
+  name,
+  description,
+}: Omit<NavigationItemProps, 'bgcolor'>) {
+  return (
+    <li
+      key={url}
+      className={classNames(
+        'flex mx-auto px-4 py-5 cursor-pointer relative w-full bg-white hover:bg-primary-7 group'
+      )}
+    >
+      <Link href={url} className="w-full mx-auto">
+        <h1 className="text-xl font-bold group-hover:text-white text-primary-8">{name}</h1>
+        <p
+          title={description}
+          className="select-none mt-1 text-left font-thin text-sm line-clamp-2 mb-3 text-neutral-4 w-2/3 group-hover:text-white"
+        >
+          {description}
+        </p>
+      </Link>
+      <EyeIcon className="absolute right-5 top-1/2 -translate-y-1/2 w-5 h-5 text-primary-8 group-hover:text-white" />
+    </li>
+  );
+}
+
+function DiscoverNavigation() {
+  return (
+    <ul className="w-full h-full flex items-start justify-start gap-y-1 flex-col overflow-y-auto primary-scrollbar">
+      {EXPLORE_NAVIGATION_LIST.map(({ name, description, url }) => (
+        <DiscoverNavigationItem key={url} {...{ name, description, url }} />
+      ))}
+    </ul>
+  );
+}
+
+function MainNavigation({ expanded }: { expanded: boolean }) {
+  return (
+    <ul
+      className={classNames(
+        'w-full h-full flex items-start justify-start gap-y-1 flex-col overflow-y-auto primary-scrollbar',
+        !expanded && 'hidden'
+      )}
+    >
+      {EXPLORE_NAVIGATION_LIST.map(({ name, url, description, bgcolor }) => (
+        <NavigationItem key={url} {...{ name, url, description, bgcolor }} />
+      ))}
+    </ul>
+  );
+}
+
+export default function Main() {
+  return (
+    <div className={classNames('relative bg-primary-9 h-full')}>
+      <div className="fixed bg-primary-9 inset-0 w-full h-full min-h-screen z-0 bg-fullbrain-image bg-blend-lighten bg-no-repeat bg-center [background-size:70%]" />
+      <div className="fixed left-0 z-20">
+        <ApplicationSidebar title={MainSideBarHeader} control={MainNavigation} />
+      </div>
+      <OBPLogo color="text-primary-5" className="!left-14" />
+      <div className="fixed top-[130px] left-[53px] max-w-[250px] w-full z-10">
+        <DefaultAccountPanel expanded />
+      </div>
+      <div className="absolute h-[calc(100vh-56px)] top-7 right-7 flex justify-end w-3/5 ml-auto z-20">
+        <DiscoverNavigation />
+      </div>
+    </div>
+  );
+}
