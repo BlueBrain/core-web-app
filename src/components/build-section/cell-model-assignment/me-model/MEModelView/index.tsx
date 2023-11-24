129fd2fcce975845eb6fcf098fa6fda114bc9073
Stefano Maximiliano Antonel <stefano.antonel@epfl.ch>
2023-11-24 08:09:19 +0000

    Keep only EModel Dropdown in me-model

--- src/components/build-section/cell-model-assignment/me-model/MEModelView/index.tsx
+++ src/components/build-section/cell-model-assignment/me-model/MEModelView/index.tsx
@@ -1,11 +1,8 @@
 import { ErrorBoundary } from 'react-error-boundary';
 
-import AssignmentAlgorithm from './AssignmentAlgorithm';
-import AxonAssignment from './AxonAssignment';
 import EModelDropdown from './EModelDropdown';
 import DefaultLoadingSuspense from '@/components/DefaultLoadingSuspense';
 import SimpleErrorComponent from '@/components/GenericErrorFallback';
-import FeatureWithEModel from '@/components/build-section/cell-model-assignment/me-model/FeatureWithEModel';
 
 export default function MEModelView() {
   return (
@@ -15,31 +12,6 @@ export default function MEModelView() {
           <EModelDropdown />
         </DefaultLoadingSuspense>
       </ErrorBoundary>
-
-      <div className="relative mt-10">
-        <div className="w-full h-full absolute flex justify-center items-center z-10 text-red-500 text-4xl">
-          <span className="-rotate-45">Coming soon</span>
-        </div>
-        <div className="flex flex-col gap-10 opacity-30">
-          <ErrorBoundary FallbackComponent={SimpleErrorComponent}>
-            <DefaultLoadingSuspense>
-              <AssignmentAlgorithm />
-            </DefaultLoadingSuspense>
-          </ErrorBoundary>
-
-          <ErrorBoundary FallbackComponent={SimpleErrorComponent}>
-            <DefaultLoadingSuspense>
-              <FeatureWithEModel />
-            </DefaultLoadingSuspense>
-          </ErrorBoundary>
-
-          <ErrorBoundary FallbackComponent={SimpleErrorComponent}>
-            <DefaultLoadingSuspense>
-              <AxonAssignment />
-            </DefaultLoadingSuspense>
-          </ErrorBoundary>
-        </div>
-      </div>
     </div>
   );
 }
