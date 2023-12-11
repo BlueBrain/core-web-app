c4ec58f96fbd916a5ecdb1353448e1c360467ccd
Bilal Meddah <bilal.meddah@epfl.ch>
2023-12-11 11:02:47 +0000

    Add border and fetch only when the morphology preview is in screen

--- src/components/explore-section/ExploreSectionListingView/MorphoThumbnail.tsx
+++ src/components/explore-section/ExploreSectionListingView/MorphoThumbnail.tsx
@@ -1,12 +1,17 @@
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { Skeleton } from 'antd';
+import { useInView } from 'react-intersection-observer';
+
 import { createHeaders } from '@/util/utils';
 import { useSessionAtomValue } from '@/hooks/hooks';
 import { BASE_URL } from '@/constants/explore-section/kg-inference';
 
 export default function MorphoThumbnail({ contentUrl }: { contentUrl: string }) {
   const session = useSessionAtomValue();
+  const { ref, inView } = useInView({
+    threshold: 0.2,
+  });
 
   const [thumbnail, setThumbnail] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
@@ -14,7 +19,7 @@ export default function MorphoThumbnail({ contentUrl }: { contentUrl: string }) 
   useEffect(() => {
     const { accessToken } = session ?? { accessToken: undefined };
 
-    if (accessToken) {
+    if (accessToken && inView) {
       setLoading(true);
 
       const encodedContentUrl = encodeURIComponent(contentUrl);
@@ -35,17 +40,21 @@ export default function MorphoThumbnail({ contentUrl }: { contentUrl: string }) 
         })
         .catch(() => setLoading(false));
     }
-  }, [contentUrl, session]);
+  }, [contentUrl, session, inView]);
 
-  return thumbnail ? (
-    <Image
-      alt={`Morphology preview: ${contentUrl}`}
-      className="mx-auto max-h-[116px]"
-      src={thumbnail}
-      width={184}
-      height={116}
-    />
-  ) : (
-    <Skeleton.Image active={loading} className="!w-[184px] !h-[116px]" />
+  return (
+    <div ref={ref} className="w-[184px] h-auto">
+      {thumbnail ? (
+        <Image
+          alt={`Morphology preview: ${contentUrl}`}
+          className="mx-auto max-h-[116px] border border-neutral-2"
+          src={thumbnail}
+          width={184}
+          height={116}
+        />
+      ) : (
+        <Skeleton.Image active={loading} className="!w-[184px] !h-[116px] rounded-none" />
+      )}
+    </div>
   );
 }
