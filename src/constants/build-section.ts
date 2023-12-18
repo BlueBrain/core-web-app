207295ad2fe03e8ea2644211c173dcc368b104a2
Pavlo Getta <pavlo.getta@epfl.ch>
2023-12-18 14:41:44 +0000

    Atlas ES API: Add missing view type param to have _search suffix

--- src/constants/build-section.ts
+++ src/constants/build-section.ts
@@ -4,4 +4,5 @@ import { composeUrl } from '@/util/nexus';
 export const ATLAS_SEARCH_URL = composeUrl('view', atlasESView.id, {
   org: atlasESView.org,
   project: atlasESView.project,
+  viewType: 'es',
 });
