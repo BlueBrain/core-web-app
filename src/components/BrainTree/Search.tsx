f665c07723b88a6304fda1619158643cdd0c8a9a
Harry Robert Taylor Anderson <harry.anderson@epfl.ch>
2023-12-20 10:48:22 +0000

    Check representation in annotation when searching for brain regions

--- src/components/BrainTree/Search.tsx
+++ src/components/BrainTree/Search.tsx
@@ -14,11 +14,10 @@ import { NavValue } from '@/state/brain-regions/types';
 import { BRAIN_VIEW_LAYER } from '@/constants/brain-hierarchy';
 
 type SearchOption = {
-  alternateViewType?: BrainViewId;
   ancestors: Record<string, BrainViewId>[];
+  label: string;
   leaves?: string[];
   representedInAnnotation: boolean;
-  label: string;
   value: string;
 };
 
@@ -40,21 +39,77 @@ export default function BrainTreeSearch({
   const brainRegions = useAtomValue(brainRegionsAtom);
   const setSelectedBrainRegion = useSetAtom(setSelectedBrainRegionAtom);
 
+  // This function returns either the hasPart or hasLayerPart array of brain region IDs, depending on the currently selected "view" (think: default or layer-based).
+  function getDescendentsFromView(
+    hasPart?: string[],
+    hasLayerPart?: string[],
+    view?: string
+  ): string[] | undefined {
+    let descendents;
+
+    // Currently, it seems that "layer" brain regions have the default view ID, even if they will never appear in the default hierarchy.
+    switch (view) {
+      case BRAIN_VIEW_LAYER:
+        descendents = hasLayerPart;
+        break;
+      default: // This means that by default, layer-based views also have the default view ID (see brainRegionOntologyAtom).
+        descendents = hasPart ?? hasLayerPart; // To compensate for this, we first check whether hasPart, before falling-back on hasLayerPart (for the layer-based brain-regions).
+    }
+
+    return descendents;
+  }
+
+  // This function's purpose is similar to that of itemsInAnnotationReducer(), in that it looks for the descendents of a brain region to recursively check whether at least one of the descendents is represented in the annotation volume.
+  // The difference is that this function relies on a flat brainRegions array, whereas itemsInAnnotationReducer() is used to handle the nested tree hierarchy structure.
+  const checkRepresentationOfDescendents = useCallback(
+    (representedInAnnotation: boolean, brainRegionId: string): boolean => {
+      if (representedInAnnotation) {
+        return representedInAnnotation; // It only needs to be true for one.
+      }
+
+      const brainRegion = brainRegions?.find(({ id }) => id === brainRegionId);
+
+      const descendents = getDescendentsFromView(
+        brainRegion?.hasPart,
+        brainRegion?.hasLayerPart,
+        brainRegion?.view
+      );
+
+      const descendentsRepresentedInAnnotation =
+        descendents?.reduce(checkRepresentationOfDescendents, false) ?? false; // If no descendents, then no descendents are represented.
+
+      return brainRegion?.representedInAnnotation ?? descendentsRepresentedInAnnotation;
+    },
+    [brainRegions]
+  );
+
   const options = useMemo(
     () =>
-      brainRegions?.map(({ title, id, isPartOf, isLayerPartOf, ...rest }) => {
-        const ancestors = getAncestors(brainRegions, id);
+      brainRegions?.reduce<SearchOption[]>(
+        (acc, { title, id, hasPart, hasLayerPart, leaves, representedInAnnotation, view }) => {
+          const descendents = getDescendentsFromView(hasPart, hasLayerPart, view);
 
-        return {
-          ancestors,
-          label: title,
-          isLayerPartOf,
-          isPartOf,
-          value: id,
-          ...rest,
-        };
-      }) ?? [],
-    [brainRegions]
+          const descendentsRepresentedInAnnotation = descendents?.reduce(
+            checkRepresentationOfDescendents,
+            false
+          );
+
+          return representedInAnnotation || descendentsRepresentedInAnnotation
+            ? [
+                ...acc,
+                {
+                  ancestors: getAncestors(brainRegions, id),
+                  label: title,
+                  leaves,
+                  representedInAnnotation,
+                  value: id,
+                },
+              ]
+            : acc;
+        },
+        []
+      ) ?? [],
+    [brainRegions, checkRepresentationOfDescendents]
   );
 
   const setSelectedAlternateViews = useSetAtom(selectedAlternateViews);
