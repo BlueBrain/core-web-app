aca93e571c473d8969cf67f6655e1b124951419e
Stefano Maximiliano Antonel <stefano.antonel@epfl.ch>
2024-01-08 13:57:34 +0000

    Select default region on Explore

---
+++ src/constants/explore-section/default-brain-region.ts
@@ -0,0 +1,10 @@
+export const defaultExploreRegion = {
+  id: 'http://api.brain-map.org/api/v2/data/Structure/567',
+  title: 'Cerebrum',
+  leaves: [],
+  representedInAnnotation: true,
+};
+
+export const defaultHierarchyTree = {
+  'http://api.brain-map.org/api/v2/data/Structure/8': null,
+};
--- src/state/brain-regions/index.ts
+++ src/state/brain-regions/index.ts
@@ -27,6 +27,11 @@ import {
 } from '@/constants/brain-hierarchy';
 import { getInitializationValue, setInitializationValue } from '@/util/utils';
 import { idAtom as brainModelConfigIdAtom } from '@/state/brain-model-config';
+import { sectionAtom } from '@/state/application';
+import {
+  defaultExploreRegion,
+  defaultHierarchyTree,
+} from '@/constants/explore-section/default-brain-region';
 
 /*
   Atom dependency graph
@@ -342,14 +347,16 @@ export const meshDistributionsAtom = atom<Promise<{ [id: string]: Mesh } | null>
 });
 
 export const selectedBrainRegionAtom = atomWithDefault<SelectedBrainRegion | null>((get) => {
-  const brainModelConfigId = get(brainModelConfigIdAtom);
-  if (!brainModelConfigId) return null;
+  const sectionName = get(sectionAtom);
+  if (!sectionName) return null;
 
   const initializationBrainRegion = getInitializationValue<DefaultBrainRegionType>(
     DEFAULT_BRAIN_REGION_STORAGE_KEY
   );
 
-  return initializationBrainRegion ? initializationBrainRegion.value : null;
+  const defaultRegion = sectionName === 'explore' ? defaultExploreRegion : null;
+
+  return initializationBrainRegion ? initializationBrainRegion.value : defaultRegion;
 });
 
 export const selectedPreBrainRegionsAtom = atom(new Map<string, string>());
@@ -444,7 +451,9 @@ export const brainRegionHierarchyStateAtom = atomWithDefault<NavValue | null>((g
   const initializationBrainRegion = getInitializationValue<DefaultBrainRegionType>(
     DEFAULT_BRAIN_REGION_STORAGE_KEY
   );
-  return initializationBrainRegion ? initializationBrainRegion.brainRegionHierarchyState : null;
+  return initializationBrainRegion
+    ? initializationBrainRegion.brainRegionHierarchyState
+    : defaultHierarchyTree;
 });
 
 brainRegionHierarchyStateAtom.debugLabel = 'brainRegionHierarchyStateAtom';
