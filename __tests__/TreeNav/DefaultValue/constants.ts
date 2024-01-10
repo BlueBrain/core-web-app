2f42a30565d0558c8650939a30ceeaa36cadd52c
Stefano Antonel <stefanoantonel@gmail.com>
2024-01-10 13:21:12 +0100

    Assign saved region to Isocortex

--- __tests__/TreeNav/DefaultValue/constants.ts
+++ __tests__/TreeNav/DefaultValue/constants.ts
@@ -2,17 +2,9 @@
 
 export const previouslySelectedRegion = {
   value: {
-    id: 'http://api.brain-map.org/api/v2/data/Structure/1057',
-    title: 'Gustatory areas',
-    leaves: [
-      'http://api.brain-map.org/api/v2/data/Structure/662',
-      'http://api.brain-map.org/api/v2/data/Structure/148',
-      'http://api.brain-map.org/api/v2/data/Structure/614454308',
-      'http://api.brain-map.org/api/v2/data/Structure/638',
-      'http://api.brain-map.org/api/v2/data/Structure/614454309',
-      'http://api.brain-map.org/api/v2/data/Structure/36',
-      'http://api.brain-map.org/api/v2/data/Structure/187',
-    ],
+    id: 'http://api.brain-map.org/api/v2/data/Structure/315',
+    title: 'Isocortex',
+    leaves: [],
     representedInAnnotation: true,
   },
   brainRegionHierarchyState: {
@@ -20,9 +12,7 @@ export const previouslySelectedRegion = {
       'http://api.brain-map.org/api/v2/data/Structure/567': {
         'http://api.brain-map.org/api/v2/data/Structure/688': {
           'http://api.brain-map.org/api/v2/data/Structure/695': {
-            'http://api.brain-map.org/api/v2/data/Structure/315': {
-              'http://api.brain-map.org/api/v2/data/Structure/95': null,
-            },
+            'http://api.brain-map.org/api/v2/data/Structure/315': null,
           },
         },
       },
