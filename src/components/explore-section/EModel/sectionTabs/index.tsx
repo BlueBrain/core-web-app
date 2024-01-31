24cd0a12c012f6c992e52365017812629746a214
Bilal Meddah <bilal.meddah@epfl.ch>
2024-01-31 12:36:33 +0100

    134-1402/update: add components for emodel

---
+++ src/components/explore-section/EModel/sectionTabs/index.tsx
@@ -0,0 +1,58 @@
+import { useMemo } from 'react';
+import { parseAsString, useQueryState } from 'nuqs';
+import { Menu } from 'antd';
+import { MenuProps } from 'antd/es/menu';
+
+export type EmodelTabKeys = 'emodel-configuration' | 'emodel-analysis' | 'emodel-simulation';
+export const EMODEL_TABS: Array<{ key: EmodelTabKeys; title: string }> = [
+  {
+    key: 'emodel-configuration',
+    title: 'Configuration',
+  },
+  {
+    key: 'emodel-analysis',
+    title: 'Analysis',
+  },
+  {
+    key: 'emodel-simulation',
+    title: 'Simulation',
+  },
+];
+
+export default function Tabs() {
+  const [activeTab, setActiveTab] = useQueryState(
+    'tab',
+    parseAsString.withDefault(EMODEL_TABS.at(0)!.key)
+  );
+  const items = useMemo(
+    () =>
+      EMODEL_TABS.map((item) => ({
+        key: item.key,
+        title: item.title,
+        label: item.title,
+        className: 'text-center font-semibold !flex-[1_1_31%]',
+        style: {
+          backgroundColor: activeTab === item.key ? '#002766' : 'white',
+          color: activeTab === item.key ? 'white' : '#002766',
+        },
+      })),
+    [activeTab]
+  );
+
+  const onClick: MenuProps['onClick'] = ({ key, domEvent }) => {
+    domEvent.preventDefault();
+    domEvent.stopPropagation();
+    setActiveTab(key);
+  };
+
+  return (
+    <Menu
+      onClick={onClick}
+      selectedKeys={[activeTab]}
+      mode="horizontal"
+      theme="dark"
+      className="flex justify-start w-full"
+      items={items}
+    />
+  );
+}
