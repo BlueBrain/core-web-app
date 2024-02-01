ebbf27c7e63b4bf3a2e3733410336edc1a6248d9
Bilal Meddah <bilal.meddah@epfl.ch>
2024-02-01 09:53:13 +0100

    add HOC for passing onClick for experiment types

---
+++ src/components/explore-section/WithExploreExperiment.tsx
@@ -0,0 +1,38 @@
+import { ReactNode } from 'react';
+import { useRouter } from 'next/navigation';
+
+import { RenderButtonProps } from './ExploreSectionListingView/WithRowSelection';
+import { OnCellClick } from './ExploreSectionListingView/ExploreSectionTable';
+import { detailUrlBuilder } from '@/util/common';
+import ExploreSectionListingView from '@/components/explore-section/ExploreSectionListingView';
+import { ExploreDataBrainRegionSource } from '@/types/explore-section/application';
+import { DataType } from '@/constants/explore-section/list-views';
+
+export default function WithExploreExperiment({
+  enableDownload,
+  dataType,
+  brainRegionSource,
+  renderButton,
+}: {
+  enableDownload?: boolean;
+  dataType: DataType;
+  brainRegionSource: ExploreDataBrainRegionSource;
+  renderButton?: (props: RenderButtonProps) => ReactNode;
+}) {
+  const router = useRouter();
+  const onCellClick: OnCellClick = (basePath, record, type) => {
+    router.push(detailUrlBuilder(basePath, record, type));
+  };
+  
+  return (
+    <ExploreSectionListingView
+      {...{
+        enableDownload,
+        dataType,
+        brainRegionSource,
+        onCellClick,
+        renderButton,
+      }}
+    />
+  );
+}
