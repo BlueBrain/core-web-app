c66a7ff9d0195a614da23fb0c798696f6d600b10
Nicolaas Smit <nick.smit@epfl.ch>
2024-02-16 11:03:11 +0000

    New morphologies dont have morphometrics this catches those cases

--- src/hooks/useMorphoMetrics.tsx
+++ src/hooks/useMorphoMetrics.tsx
@@ -6,6 +6,7 @@ import { DetailProps } from '@/types/explore-section/application';
 import { isNeuronMorphologyFeatureAnnotation } from '@/util/explore-section/typeUnionTargetting';
 import EXPLORE_FIELDS_CONFIG from '@/constants/explore-section/fields-config';
 import { FlattenedExploreESResponse } from '@/types/explore-section/es';
+import { NO_DATA_STRING } from '@/constants/explore-section/queries';
 
 export const useMorphometrics = (
   dataType: DataType,
@@ -20,20 +21,20 @@ export const useMorphometrics = (
     if (!metrics) return null;
 
     const fieldObj = EXPLORE_FIELDS_CONFIG[field.field];
+    const metricSource = metrics?.find((metric) =>
+      metric?._source && isNeuronMorphologyFeatureAnnotation(metric._source)
+        ? metric._source.compartment === metricType
+        : -1
+    );
 
     return (
       <div className="mr-10 text-primary-8">
         {showLabel && <div className="uppercase text-neutral-4">{fieldObj.title}</div>}
         <div className={`${showLabel ? 'mt-2' : 'ml-6 mt-0'}`}>
           <div key={field.field} className={`mb-2 h-6 truncate ${field.className}`}>
-            {fieldObj?.render?.esResourceViewFn?.(
-              'text',
-              metrics.find((metric) =>
-                isNeuronMorphologyFeatureAnnotation(metric._source)
-                  ? metric._source.compartment === metricType
-                  : -1
-              )
-            )}
+            {metricSource
+              ? fieldObj?.render?.esResourceViewFn?.('text', metricSource)
+              : NO_DATA_STRING}
           </div>
         </div>
       </div>
