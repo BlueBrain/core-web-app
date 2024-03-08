ad1aa7334bad1d9c63c4b271da0679e2bcee19e0
Konstantinos Platis <konstantinos.platis@epfl.ch>
2024-03-08 11:07:58 +0100

    Add interactive page

--- src/app/virtual-lab/lab/[virtualLabId]/(project)/project/[projectId]/(explore)/explore/(interactive)/interactive/layout.tsx
+++ src/app/virtual-lab/lab/[virtualLabId]/(project)/project/[projectId]/(explore)/explore/(interactive)/interactive/layout.tsx
@@ -3,11 +3,11 @@
 import { ReactNode, useEffect } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import { useSetAtom } from 'jotai';
-
 import SimpleErrorComponent from '@/components/GenericErrorFallback';
 import SideMenu from '@/components/SideMenu';
 import { idAtom as brainModelConfigIdAtom } from '@/state/brain-model-config';
 import { defaultModelRelease } from '@/config';
+
 import { useSetBrainRegionFromQuery } from '@/hooks/brain-region-panel';
 import { BrainRegionsSidebar } from '@/components/build-section/BrainRegionSelector';
 
@@ -23,23 +23,21 @@ export default function VirtualLabProjectInteractiveExploreLayout({
   useEffect(() => setConfigId(defaultModelRelease.id), [setConfigId]);
 
   return (
-    <div className="grid h-screen grid-cols-[min-content_min-content_auto] grid-rows-1">
+    <div className="grid grid-cols-[min-content_min-content_auto] grid-rows-1">
       <ErrorBoundary FallbackComponent={SimpleErrorComponent}>
         <div className="flex flex-row gap-4">
           <SideMenu
             links={[
               {
-                key: 'project',
-                href: '/virtual-lab/lab/test/project/test',
-                content: 'Name of the project',
-              },
-              {
-                key: 'explore',
-                href: '/virtual-lab/lab/test/project/test/explore',
-                content: 'Explore',
+                key: 'virtual-lab',
+                href: '/virtual-lab/lab/test',
+                content: (
+                  <div className="flex gap-2 font-bold">
+                    <span className="text-primary-3">Virtual lab:</span>Institute of Neuroscience
+                  </div>
+                ),
               },
             ]}
-            current="explore"
           />
         </div>
       </ErrorBoundary>
