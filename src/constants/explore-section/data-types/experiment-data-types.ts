9f6ddf18575453b2bf9ddc8b52b1e4273300575f
Konstantinos Platis <konstantinos.platis@epfl.ch>
2024-03-11 16:44:04 +0100

    Add page

--- src/constants/explore-section/data-types/experiment-data-types.ts
+++ src/constants/explore-section/data-types/experiment-data-types.ts
@@ -1,8 +1,8 @@
 import { DataType } from '@/constants/explore-section/list-views';
 import { Field } from '@/constants/explore-section/fields-config/enums';
-import { DataTypeGroup } from '@/types/explore-section/data-types';
+import { DataTypeConfig, DataTypeGroup } from '@/types/explore-section/data-types';
 
-export const EXPERIMENT_DATA_TYPES = {
+export const EXPERIMENT_DATA_TYPES: { [key: string]: DataTypeConfig } = {
   [DataType.ExperimentalNeuronMorphology]: {
     title: 'Morphology',
     group: DataTypeGroup.ExperimentalData,
