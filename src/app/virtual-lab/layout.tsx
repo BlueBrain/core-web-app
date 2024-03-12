984d81617dd6be2cdb685eee817b0e24bbbc7066
Harry Robert Taylor Anderson <harry.anderson@epfl.ch>
2024-03-12 11:43:09 +0000

    Remove space

--- src/app/virtual-lab/layout.tsx
+++ src/app/virtual-lab/layout.tsx
@@ -6,7 +6,7 @@ import SimpleErrorComponent from '@/components/GenericErrorFallback';
 
 export default function VirtualLabLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="h-screen overflow-y-scroll bg-primary-9 text-white ">
+    <div className="h-screen overflow-y-scroll bg-primary-9 text-white">
       <ErrorBoundary FallbackComponent={SimpleErrorComponent}>{children}</ErrorBoundary>
     </div>
   );
