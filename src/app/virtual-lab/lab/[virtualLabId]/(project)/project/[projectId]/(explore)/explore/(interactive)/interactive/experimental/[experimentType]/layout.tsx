1416f9f0239affd1c905d250b9d914213e9262eb
Konstantinos Platis <konstantinos.platis@epfl.ch>
2024-03-12 09:55:43 +0100

    Fix links

--- src/app/virtual-lab/lab/[virtualLabId]/(project)/project/[projectId]/(explore)/explore/(interactive)/interactive/experimental/[experimentType]/layout.tsx
+++ src/app/virtual-lab/lab/[virtualLabId]/(project)/project/[projectId]/(explore)/explore/(interactive)/interactive/experimental/[experimentType]/layout.tsx
@@ -3,6 +3,6 @@
 import { ReactNode } from 'react';
 import ExploreListingLayout from '@/components/explore-section/ExploreListingLayout';
 
-export default function ExploreInteractiveDataLayout({ children }: { children: ReactNode }) {
+export default function VirtualLabExperimentLayout({ children }: { children: ReactNode }) {
   return <ExploreListingLayout>{children}</ExploreListingLayout>;
 }
