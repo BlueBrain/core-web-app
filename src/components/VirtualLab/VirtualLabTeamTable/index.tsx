7a577463aafd31a75d4316f10319e065709b9292
Konstantinos Platis <konstantinos.platis@epfl.ch>
2024-03-14 10:43:54 +0100

    Add random color generator

--- src/components/VirtualLab/VirtualLabTeamTable/index.tsx
+++ src/components/VirtualLab/VirtualLabTeamTable/index.tsx
@@ -28,6 +28,28 @@ export default function VirtualLabTeamTable() {
     },
   ];
 
+  const getInitials = (name: string) => {
+    const splitted = name.split(' ');
+    return `${splitted[0][0]}${splitted[1][0]}`;
+  };
+
+  const generateRandomHexColor = () => {
+    // Generating random RGB values
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+
+    // Converting RGB to hexadecimal
+    const hexRed = red.toString(16).padStart(2, '0');
+    const hexGreen = green.toString(16).padStart(2, '0');
+    const hexBlue = blue.toString(16).padStart(2, '0');
+
+    // Concatenating hexadecimal values
+    const hexColor = `#${hexRed}${hexGreen}${hexBlue}`;
+
+    return hexColor;
+  };
+
   const columns = [
     {
       title: 'Icon',
@@ -36,9 +58,10 @@ export default function VirtualLabTeamTable() {
       render: (_: any, record: Member) => {
         return (
           <div
-            className={`flex h-12 w-12 items-center justify-center bg-blue-500 ${record.role === 'member' ? 'rounded-full' : ''}`}
+            style={{ backgroundColor: generateRandomHexColor() }}
+            className={`flex h-12 w-12 items-center justify-center ${record.role === 'member' ? 'rounded-full' : ''}`}
           >
-            <span className="text-2xl font-bold text-white">AB</span>
+            <span className="text-2xl font-bold text-white">{getInitials(record.name)}</span>
           </div>
         );
       },
