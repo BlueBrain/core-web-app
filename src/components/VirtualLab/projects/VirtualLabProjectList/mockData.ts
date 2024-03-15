3f50a5865cc67b3e315a5caa8e8fb4e7abade933
Konstantinos Platis <konstantinos.platis@epfl.ch>
2024-03-15 12:14:24 +0100

    Initial creation

---
+++ src/components/VirtualLab/projects/VirtualLabProjectList/mockData.ts
@@ -0,0 +1,37 @@
+import { Project } from './types';
+
+export const mockProjects: Project[] = [
+  {
+    title: 'Thalamus exploration project 1',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tempor enim nec condimentum varius. Suspendisse quis sem efficitur, lacinia enim eu, facilisis leo. Aliquam ex arcu, aliquet et sagittis ac, imperdiet a diam. Fusce sodales, sapien ut mollis faucibus, nisi ex fringilla tellus, eu sagittis ipsum neque eu justo. Suspendisse potenti. Mauris a pellentesque arcu. Ut accumsan viverra nibh, vel condimentum ipsum semper quis. In venenatis vel nulla ut tempor. Mauris libero mi, mattis eget iaculis ac, vulputate id augue. Sed ullamcorper, erat ut euismod congue, lorem diam volutpat lectus, id tempus mi diam nec est. Aenean eu libero a. ',
+    exploreSessions: 278,
+    builds: 278,
+    simulationExperiments: 15,
+    members: 5,
+    Admin: 'Julian Budd',
+    creationDate: '12.02.2023',
+  },
+  {
+    title: 'Project Hippocampus',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tempor enim nec condimentum varius. Suspendisse quis sem efficitur, lacinia enim eu, facilisis leo. Aliquam ex arcu, aliquet et sagittis ac, imperdiet a diam. Fusce sodales, sapien ut mollis faucibus, nisi ex fringilla tellus, eu sagittis ipsum neque eu justo. Suspendisse potenti. Mauris a pellentesque arcu. Ut accumsan viverra nibh, vel condimentum ipsum semper quis. In venenatis vel nulla ut tempor. Mauris libero mi, mattis eget iaculis ac, vulputate id augue. Sed ullamcorper, erat ut euismod congue, lorem diam volutpat lectus, id tempus mi diam nec est. Aenean eu libero a. ',
+    exploreSessions: 278,
+    builds: 278,
+    simulationExperiments: 15,
+    members: 5,
+    Admin: 'Julian Budd',
+    creationDate: '12.02.2023',
+  },
+  {
+    title: 'Isocortex exploration project',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tempor enim nec condimentum varius. Suspendisse quis sem efficitur, lacinia enim eu, facilisis leo. Aliquam ex arcu, aliquet et sagittis ac, imperdiet a diam. Fusce sodales, sapien ut mollis faucibus, nisi ex fringilla tellus, eu sagittis ipsum neque eu justo. Suspendisse potenti. Mauris a pellentesque arcu. Ut accumsan viverra nibh, vel condimentum ipsum semper quis. In venenatis vel nulla ut tempor. Mauris libero mi, mattis eget iaculis ac, vulputate id augue. Sed ullamcorper, erat ut euismod congue, lorem diam volutpat lectus, id tempus mi diam nec est. Aenean eu libero a. ',
+    exploreSessions: 278,
+    builds: 278,
+    simulationExperiments: 15,
+    members: 5,
+    Admin: 'Julian Budd',
+    creationDate: '12.02.2023',
+  },
+];
---
+++ src/components/VirtualLab/projects/VirtualLabProjectList/types.ts
@@ -0,0 +1,12 @@
+
+
+export type Project = {
+    title: string;
+    description: string;
+    exploreSessions: number;
+    builds: number;
+    simulationExperiments: number;
+    members: number;
+    Admin: string;
+    creationDate: string;
+}
