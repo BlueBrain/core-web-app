b69b968ac75f29a431d7d66944fbc0712f69110c
Konstantinos Platis <konstantinos.platis@epfl.ch>
2024-03-18 14:59:40 +0100

    Add stats

--- src/components/VirtualLab/VirtualLabHomePage/index.tsx
+++ src/components/VirtualLab/VirtualLabHomePage/index.tsx
@@ -1,26 +1,51 @@
-import { EditOutlined } from '@ant-design/icons';
+import { CalendarOutlined, EditOutlined, UserOutlined } from '@ant-design/icons';
+
+import VirtualLabStatistic from '../VirtualLabStatistic';
+import { mockVirtualLab } from '../mockData/project';
+import { MembersGroupIcon, StatsEditIcon } from '@/components/icons';
+import Brain from '@/components/icons/Brain';
 
 export default function VirtualLabHomePage() {
+  const iconStyle = { color: '#69C0FF' };
+  const virtualLab = mockVirtualLab;
   return (
-    <div className="mt-10 bg-primary-8 p-8">
+    <div className="mt-10 flex flex-col gap-4 bg-primary-8 p-8">
       <div className="flex flex-row justify-between">
         <div className="max-w-[50%]">
           <div className="text-primary-2">Virtual Lab name</div>
-          <h2 className="text-4xl font-bold">Institute of Neuroscience</h2>
-          <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tempor enim nec
-            condimentum varius. Suspendisse quis sem efficitur, lacinia enim eu, facilisis leo.
-            Aliquam ex arcu, aliquet et sagittis ac, imperdiet a diam. Fusce sodales, sapien ut
-            mollis faucibus, nisi ex fringilla tellus, eu sagittis ipsum neque eu justo. Suspendisse
-            potenti. Mauris a pellentesque arcu. Ut accumsan viverra nibh, vel condimentum ipsum
-            semper quis. In venenatis vel nulla ut tempor. Mauris libero mi, mattis eget iaculis ac,
-            vulputate id augue. Sed ullamcorper, erat ut euismod congue, lorem diam volutpat lectus,
-            id tempus mi diam nec est. Aenean eu libero a.
-          </div>
+          <h2 className="text-4xl font-bold">{virtualLab.title}</h2>
+          <div>{virtualLab.description}</div>
         </div>
         <div>
           <EditOutlined />
         </div>
+      </div>
+      <div className="flex gap-5">
+        <VirtualLabStatistic
+          icon={<Brain style={iconStyle} />}
+          title="Builds"
+          detail={virtualLab.builds}
+        />
+        <VirtualLabStatistic
+          icon={<StatsEditIcon style={iconStyle} />}
+          title="Simulation experiments"
+          detail={virtualLab.simulationExperiments}
+        />
+        <VirtualLabStatistic
+          icon={<UserOutlined style={iconStyle} />}
+          title="Members"
+          detail={virtualLab.members}
+        />
+        <VirtualLabStatistic
+          icon={<MembersGroupIcon style={iconStyle} />}
+          title="Admin"
+          detail={virtualLab.admin}
+        />
+        <VirtualLabStatistic
+          icon={<CalendarOutlined style={iconStyle} />}
+          title="Creation date"
+          detail={virtualLab.creationDate}
+        />
       </div>
     </div>
   );
---
+++ src/components/VirtualLab/mockData/project.ts
@@ -0,0 +1,20 @@
+type VirtualLab = {
+  title: string;
+  description: string;
+  builds: number;
+  simulationExperiments: number;
+  members: number;
+  admin: string;
+  creationDate: string;
+};
+
+export const mockVirtualLab: VirtualLab = {
+  title: 'Institute of Neuroscience',
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tempor enim nec condimentum varius. Suspendisse quis sem efficitur, lacinia enim eu, facilisis leo. Aliquam ex arcu, aliquet et sagittis ac, imperdiet a diam. Fusce sodales, sapien ut mollis faucibus, nisi ex fringilla tellus, eu sagittis ipsum neque eu justo. Suspendisse potenti. Mauris a pellentesque arcu. Ut accumsan viverra nibh, vel condimentum ipsum semper quis. In venenatis vel nulla ut tempor. Mauris libero mi, mattis eget iaculis ac, vulputate id augue. Sed ullamcorper, erat ut euismod congue, lorem diam volutpat lectus, id tempus mi diam nec est. Aenean eu libero a.',
+  builds: 278,
+  simulationExperiments: 15,
+  members: 9,
+  admin: 'Julian Budd',
+  creationDate: '12.02.2023',
+};
