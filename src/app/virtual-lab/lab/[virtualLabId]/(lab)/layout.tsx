ea756a18d7784635a383f4e5612f98e331832dba
Konstantinos Platis <konstantinos.platis@epfl.ch>
2024-03-20 15:57:21 +0100

    Fix padding

--- src/app/virtual-lab/lab/[virtualLabId]/(lab)/layout.tsx
+++ src/app/virtual-lab/lab/[virtualLabId]/(lab)/layout.tsx
@@ -8,7 +8,7 @@ import VirtualLabTopMenu from '@/components/VirtualLab/VirtualLabTopMenu';
 
 export default function VirtualLabPageLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="inset-0 z-0 grid grid-cols-[1fr_3fr] grid-rows-1 overflow-y-scroll bg-primary-9 bg-center px-10 pt-10 text-white">
+    <div className="inset-0 z-0 grid grid-cols-[1fr_3fr] grid-rows-1 overflow-y-scroll bg-primary-9 bg-center p-10 text-white">
       <ErrorBoundary FallbackComponent={SimpleErrorComponent}>
         <div className="flex flex-row gap-4">
           <VirtualLabSidebar />
