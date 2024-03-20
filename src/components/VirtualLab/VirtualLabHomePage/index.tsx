5a1d536c5fb3a446d718e58c4b71012bfe2defdc
Konstantinos Platis <konstantinos.platis@epfl.ch>
2024-03-20 11:36:55 +0100

    Add all 3

--- src/components/VirtualLab/VirtualLabHomePage/index.tsx
+++ src/components/VirtualLab/VirtualLabHomePage/index.tsx
@@ -6,6 +6,7 @@ import VirtualLabStatistic from '../VirtualLabStatistic';
 import DiscoverObpItem from './DiscoverObpItem';
 import { mockVirtualLab } from './mockProject';
 import BudgetPanel from './BudgetPanel';
+import { basePath } from '@/config';
 import { MembersGroupIcon, StatsEditIcon } from '@/components/icons';
 import Brain from '@/components/icons/Brain';
 
@@ -61,9 +62,9 @@ export default function VirtualLabHomePage() {
       />
       <div className="mt-10 flex flex-col gap-5">
         <div className="font-bold uppercase">Discover OBP</div>
-        <div className="flex">
+        <div className="flex flex-row gap-3">
           <DiscoverObpItem
-            imagePath="/images/obp_whole_brain.webp"
+            imagePath={`${basePath}/images/virtual-lab/obp_full_brain_blue.png`}
             title="Explore"
             subtitle="How do I explore?"
             body={
@@ -78,6 +79,42 @@ export default function VirtualLabHomePage() {
               </ul>
             }
             buttonText="Discover Explore"
+            buttonHref="/"
+          />
+          <DiscoverObpItem
+            imagePath={`${basePath}/images/virtual-lab/obp_vl_build.png`}
+            title="Build"
+            subtitle="How can I build models?"
+            body={
+              <ul className="list-inside list-disc">
+                <li>Vivamus sagittis lacus vel augue faucibus dolor auctor.</li>
+                <li>Curabitur blandit tempus porttitor.</li>
+                <li>Donec sed odio dui.</li>
+                <li>
+                  Vestibulum id ligula porta felis euismod semper. Morbi leo risus, porta ac
+                  consectetur ac, vestibulum at eros.
+                </li>
+              </ul>
+            }
+            buttonText="Discover Models"
+            buttonHref="/"
+          />
+          <DiscoverObpItem
+            imagePath={`${basePath}/images/virtual-lab/obp_vl_simulate.png`}
+            title="Simulate"
+            subtitle="How can I launch simulations?"
+            body={
+              <ul className="list-inside list-disc">
+                <li>Vivamus sagittis lacus vel augue faucibus dolor auctor.</li>
+                <li>Curabitur blandit tempus porttitor.</li>
+                <li>Donec sed odio dui.</li>
+                <li>
+                  Vestibulum id ligula porta felis euismod semper. Morbi leo risus, porta ac
+                  consectetur ac, vestibulum at eros.
+                </li>
+              </ul>
+            }
+            buttonText="Discover Simulations"
             buttonHref="/"
           />
         </div>
