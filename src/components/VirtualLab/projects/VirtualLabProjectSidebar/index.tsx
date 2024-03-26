c2f4744868f94f914545fa84278485a60b988b93
Konstantinos Platis <konstantinos.platis@epfl.ch>
2024-03-26 11:21:13 +0100

    Add project switcher

--- src/components/VirtualLab/projects/VirtualLabProjectSidebar/index.tsx
+++ src/components/VirtualLab/projects/VirtualLabProjectSidebar/index.tsx
@@ -1,9 +1,17 @@
 import { usePathname } from 'next/navigation';
 
+import { ConfigProvider, Select } from 'antd';
+import { SwapOutlined } from '@ant-design/icons';
+import { mockProjects } from '../../mockData/projects';
 import VerticalLinks, { LinkItem } from '@/components/VerticalLinks';
 
 export default function VirtualLabProjectSidebar() {
   const currentPage = usePathname().split('/').pop();
+  const projectOptions = mockProjects.map((project) => ({
+    value: project.id,
+    label: project.title,
+  }));
+
   const linkItems: LinkItem[] = [
     { key: 'home', content: 'Project Home', href: 'home' },
     { key: 'library', content: 'Project Library', href: 'library' },
@@ -13,12 +21,34 @@ export default function VirtualLabProjectSidebar() {
     { key: 'simulate', content: 'Simulate', href: 'simulate' },
   ];
   return (
-    <div className="mt-10">
-      <h1 className="leading-12 mb-5 text-5xl font-bold uppercase">
+    <div className="mt-10 flex flex-col gap-5">
+      <h1 className="leading-12 text-5xl font-bold uppercase text-primary-5">
         Thalamus <br />
         Exploration <br />
         Project 1
       </h1>
+      <ConfigProvider
+        theme={{
+          components: {
+            Select: {
+              colorBgContainer: '#002766',
+              colorBgElevated: '#002766',
+              colorBorder: '#0050B3',
+              colorText: '#69C0FF',
+              optionSelectedBg: '#002766',
+              borderRadius: 0,
+              controlHeight: 40,
+            },
+          },
+        }}
+      >
+        <Select
+          suffixIcon={<SwapOutlined style={{ color: '#40A9FF' }} />}
+          defaultValue={projectOptions[0].value}
+          onChange={() => {}}
+          options={projectOptions}
+        />
+      </ConfigProvider>
 
       <VerticalLinks links={linkItems} currentPage={currentPage} />
     </div>
