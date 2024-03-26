f668e50592688d69cb1a9c97f9d307f7c08afe31
Konstantinos Platis <konstantinos.platis@epfl.ch>
2024-03-26 11:07:33 +0100

    Add select

--- src/components/VirtualLab/VirtualLabSettingsComponent/VirtualLabSidebar.tsx
+++ src/components/VirtualLab/VirtualLabSettingsComponent/VirtualLabSidebar.tsx
@@ -1,11 +1,18 @@
 'use client';
 
 import { usePathname } from 'next/navigation';
+import { ConfigProvider, Select } from 'antd';
+import { SwapOutlined } from '@ant-design/icons';
 
 import VerticalLinks, { LinkItem } from '@/components/VerticalLinks';
 
 export default function VirtualLabSidebar() {
   const currentPage = usePathname().split('/').pop();
+
+  const virtualLabOptions: { value: string; label: string }[] = [
+    { value: '1', label: 'Institute of Neuroscience' },
+    { value: '2', label: 'Neural Blue Lab' },
+  ];
 
   const linkItems: LinkItem[] = [
     { key: 'lab', content: 'The Virtual Lab', href: 'lab' },
@@ -14,8 +21,30 @@ export default function VirtualLabSidebar() {
     { key: 'admin', content: 'Admin', href: 'admin' },
   ];
   return (
-    <div className="flex flex-col gap-5">
+    <div className="mr-12 flex flex-col gap-5">
       <div className="text-5xl font-bold uppercase text-primary-5">Institute of Neuroscience</div>
+      <ConfigProvider
+        theme={{
+          components: {
+            Select: {
+              colorBgContainer: '#002766',
+              colorBgElevated: '#002766',
+              colorBorder: '#0050B3',
+              colorText: 'rgb(255, 255, 255)',
+              optionSelectedBg: '#002766',
+              borderRadius: 0,
+              controlHeight: 40,
+            },
+          },
+        }}
+      >
+        <Select
+          suffixIcon={<SwapOutlined style={{ color: '#40A9FF' }} />}
+          defaultValue="1"
+          onChange={() => {}}
+          options={virtualLabOptions}
+        />
+      </ConfigProvider>
       <VerticalLinks links={linkItems} currentPage={currentPage} />
     </div>
   );
