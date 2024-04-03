0af18a338e055198313803172845cc9b169dd899
Konstantinos Platis <konstantinos.platis@epfl.ch>
2024-04-03 15:25:07 +0200

    Change layout

---
+++ src/app/virtual-lab/(main)/layout.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { ReactNode } from 'react';
+import VirtualLabTopMenu from '@/components/VirtualLab/VirtualLabTopMenu';
+
+export default function MainVirtualLabLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className="flex h-full flex-col p-6">
+      <div className="flex w-full justify-between">
+        <div className="w-[100px] text-3xl font-bold">Open Brain Platform</div>
+        <VirtualLabTopMenu />
+      </div>
+      <div className="h-full">{children}</div>
+    </div>
+  );
+}
--- src/components/VirtualLab/VirtualLabTopMenu/index.tsx
+++ src/components/VirtualLab/VirtualLabTopMenu/index.tsx
@@ -6,19 +6,26 @@ export default function VirtualLabTopMenu() {
   const { data: session } = useSession();
 
   return (
-    <div className="flex h-14 w-full justify-end">
-      <div className="flex w-fit justify-end border border-primary-7">
-        <Link className="w-48 border-r border-primary-7 p-4 font-bold" href="/getting-started">
-          Getting Started
-        </Link>
-        <Link className="w-48 border-r border-primary-7 p-4 font-bold" href="/about">
-          About
-        </Link>
-        <div className="w-48 border-r border-primary-7 p-4 font-bold">
-          <UserOutlined className="mr-2 text-primary-4" />
-          <span className="font-bold">{session?.user.name}</span>
-        </div>
-      </div>
+    <div className="flex h-14 w-fit justify-end border border-primary-7">
+      <Link className="w-48 border-r border-primary-7 p-4 font-bold" href="/getting-started">
+        Getting Started
+      </Link>
+      <Link className="w-48 border-r border-primary-7 p-4 font-bold" href="/about">
+        About
+      </Link>
+      <Link
+        className="w-48 border-r border-primary-7 p-4 font-bold"
+        href={session?.user ? 'user' : 'login'}
+      >
+        {session?.user ? (
+          <>
+            <UserOutlined className="mr-2 text-primary-4" />
+            <span className="font-bold">{session?.user.name}</span>
+          </>
+        ) : (
+          <>Login</>
+        )}
+      </Link>
     </div>
   );
 }
