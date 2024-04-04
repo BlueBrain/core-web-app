a53a5da4939031df09b81d7337b130a6b6eb126e
Nicolaas Smit <nick.smit@epfl.ch>
2024-04-04 10:55:35 +0000

    Implements link to literature from virtual labs projects page.

---
+++ src/app/virtual-lab/lab/[virtualLabId]/(project)/project/[projectId]/(explore)/explore/(main)/(literature)/literature/layout.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { ReactNode } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+
+import SimpleErrorComponent from '@/components/GenericErrorFallback';
+import Sidebar from '@/components/explore-section/Sidebar';
+import { BrainRegionsSidebar } from '@/components/build-section/BrainRegionSelector';
+import { useSetBrainRegionFromQuery } from '@/hooks/brain-region-panel';
+
+type LiteratureLayoutProps = {
+  children: ReactNode;
+};
+
+export default function GenericLayout({ children }: LiteratureLayoutProps) {
+  useSetBrainRegionFromQuery();
+
+  return (
+    <div className="grid h-screen grid-cols-[min-content_min-content_auto] grid-rows-1 bg-white">
+      <ErrorBoundary FallbackComponent={SimpleErrorComponent}>
+        <Sidebar />
+      </ErrorBoundary>
+      <ErrorBoundary FallbackComponent={SimpleErrorComponent}>
+        <BrainRegionsSidebar />
+      </ErrorBoundary>
+      {children}
+    </div>
+  );
+}
