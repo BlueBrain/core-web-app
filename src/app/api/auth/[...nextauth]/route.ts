029ae9f5fd1ae788dbbb340e43e57b80abf45a4f
Dinika Saxena <dinikasaxenas@gmail.com>
2024-04-04 17:54:48 +0200

    134 // Accept invite in frontend
    Signed-off-by: Dinika Saxena <dinikasaxenas@gmail.com>

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,4 +4,4 @@ import { authOptions } from '@/auth';
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST, authOptions };
