cb3cd7f64a4b67d1560048458a149adceef34e2e
Konstantinos Platis <konstantinos.platis@epfl.ch>
2024-04-15 11:46:47 +0200

    Make vl project user fetching a hoc

--- src/app/virtual-lab/lab/[virtualLabId]/(project)/project/[projectId]/(pages)/team/page.tsx
+++ src/app/virtual-lab/lab/[virtualLabId]/(project)/project/[projectId]/(pages)/team/page.tsx
@@ -1,9 +1,17 @@
+'use client';
+
 import VirtualLabTeamTable from '@/components/VirtualLab/VirtualLabTeamTable';
+import withVirtualLabProjectUsers from '@/components/VirtualLab/data/WithVirtualLabProjectUsers';
 import { ServerSideComponentProp } from '@/types/common';
 
 export default function VirtualLabProjectTeamPage({
   params,
 }: ServerSideComponentProp<{ virtualLabId: string; projectId: string }>) {
   const { virtualLabId, projectId } = params;
-  return <VirtualLabTeamTable virtualLabId="to-be-replaced-by-actual-id" />;
+  const WithVirtualLabProjectUsers = withVirtualLabProjectUsers(
+    VirtualLabTeamTable,
+    virtualLabId,
+    projectId
+  );
+  return <WithVirtualLabProjectUsers />;
 }
