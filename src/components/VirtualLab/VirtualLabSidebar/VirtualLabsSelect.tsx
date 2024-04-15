2d56a49f63d346a54a69d5e0e284d702ba55bae6
Konstantinos Platis <konstantinos.platis@epfl.ch>
2024-04-15 13:54:14 +0200

    Modify to button

--- src/components/VirtualLab/VirtualLabSidebar/VirtualLabsSelect.tsx
+++ src/components/VirtualLab/VirtualLabSidebar/VirtualLabsSelect.tsx
@@ -1,52 +1,14 @@
 'use client';
 
-import { ConfigProvider, Select, Spin } from 'antd';
-import { LoadingOutlined, SwapOutlined } from '@ant-design/icons';
-import { useAtomValue } from 'jotai';
-import { loadable } from 'jotai/utils';
-import { usePathname, useRouter } from 'next/navigation';
-import { virtualLabsOfUserAtom } from '@/state/virtual-lab/lab';
+import { SwapOutlined } from '@ant-design/icons';
 
 export default function VirtualLabsSelect() {
-  const virtualLabs = useAtomValue(loadable(virtualLabsOfUserAtom));
-  const router = useRouter();
-  const path = usePathname();
-
-  if (virtualLabs.state === 'loading') {
-    return <Spin indicator={<LoadingOutlined />} />;
-  }
-  if (virtualLabs.state === 'hasError') {
-    return null;
-  }
-
-  const virtualLabOptions: { value: string; label: string }[] = virtualLabs.data.results.map(
-    (vl) => ({ value: vl.id, label: vl.name })
-  );
-
   return (
-    <ConfigProvider
-      theme={{
-        components: {
-          Select: {
-            colorBgContainer: '#002766',
-            colorBgElevated: '#002766',
-            colorBorder: '#0050B3',
-            colorText: 'rgb(255, 255, 255)',
-            optionSelectedBg: '#002766',
-            borderRadius: 0,
-            controlHeight: 40,
-          },
-        },
-      }}
+    <a
+      href="/virtual-lab/select"
+      className="flex items-center justify-between border border-primary-7 p-3 text-primary-3"
     >
-      <Select
-        suffixIcon={<SwapOutlined style={{ color: '#40A9FF' }} />}
-        defaultValue="Switch Virtual Lab"
-        onChange={(selected) => {
-          router.push(`/virtual-lab/lab/${selected}/${path.split('/').reverse()[0]}`);
-        }}
-        options={virtualLabOptions}
-      />
-    </ConfigProvider>
+      <span>Switch Virtual Lab</span> <SwapOutlined />
+    </a>
   );
 }
