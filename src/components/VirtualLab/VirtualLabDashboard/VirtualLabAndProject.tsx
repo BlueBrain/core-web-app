e4f98f452b922f5dd895b740e3823cbe866cd07b
Konstantinos Platis <konstantinos.platis@epfl.ch>
2024-04-16 16:37:01 +0200

    Add virtual lab project banner as separate component

--- src/components/VirtualLab/VirtualLabDashboard/VirtualLabAndProject.tsx
+++ src/components/VirtualLab/VirtualLabDashboard/VirtualLabAndProject.tsx
@@ -1,4 +1,10 @@
+import { useAtomValue } from 'jotai';
+import { loadable } from 'jotai/utils';
+import { Spin } from 'antd';
+import { useCallback } from 'react';
+import { LoadingOutlined } from '@ant-design/icons';
 import VirtualLabBanner from '../VirtualLabBanner';
+import { virtualLabProjectsAtomFamily } from '@/state/virtual-lab/projects';
 
 type Props = {
   id: string;
@@ -8,5 +14,29 @@ type Props = {
 };
 
 export default function VirtualLabAndProject({ id, name, description, createdAt }: Props) {
-  return <VirtualLabBanner id={id} name={name} description={description} createdAt={createdAt} />;
+  const projects = useAtomValue(loadable(virtualLabProjectsAtomFamily(id)));
+
+  const renderProjects = useCallback(() => {
+    if (projects.state === 'loading') {
+      return <Spin indicator={<LoadingOutlined />} />;
+    }
+    if (projects.state === 'hasData') {
+      return projects.data.results.map((project) => <div>project</div>);
+    }
+
+    return null;
+  }, []);
+
+  return (
+    <div>
+      <VirtualLabBanner
+        id={id}
+        name={name}
+        description={description}
+        createdAt={createdAt}
+        withLink
+      />
+      <div className="ml-5">{renderProjects()}</div>
+    </div>
+  );
 }
