7ddb0794bd468bbc0e5113b7a20e5514d650e6b1
Nick Smit <nick.smit@epfl.ch>
2024-04-30 14:45:13 +0200

    removed duplicate button

--- src/app/virtual-lab/(home)/layout.tsx
+++ src/app/virtual-lab/(home)/layout.tsx
@@ -5,14 +5,19 @@ import { ErrorBoundary } from 'react-error-boundary';
 import { PlusOutlined } from '@ant-design/icons';
 
 import { ModalStateProvider } from '@/components/VirtualLab/create/contexts/ModalStateContext';
+import { useModalState } from '@/components/VirtualLab/create/contexts/ModalStateContext';
+
 import SimpleErrorComponent from '@/components/GenericErrorFallback';
 import VirtualLabTopMenu from '@/components/VirtualLab/VirtualLabTopMenu';
 
 export default function VirtualLabPageLayout({ children }: { children: ReactNode }) {
+  const { showModal } = useModalState();
+
   const extraHeaderItems: ReactNode[] = [
     <div
       key={1}
       className="flex w-52 flex-row justify-between border-r border-primary-7 p-4 font-bold"
+      onClick={showModal}
     >
       <span className="font-bold">Create virtual lab</span>
       <PlusOutlined />
