d6ad091e761b984560ac757fd8644e0fb7e8ba7b
Nick Smit <nick.smit@epfl.ch>
2024-04-30 13:48:30 +0200

    removed unused type

--- src/components/VirtualLab/create/types.ts
+++ src/components/VirtualLab/create/types.ts
@@ -1,9 +1,7 @@
 import { VirtualLab } from '@/types/virtual-lab/lab';
-import { KeysOfType } from '@/util/typing';
 
 export type VirtualLabWithOptionalId = Omit<VirtualLab, 'id'> & { id?: string };
 
-export type VirtualLabStringKeys = KeysOfType<VirtualLabWithOptionalId, string>;
 export interface FieldType {
   label: string;
   placeholder: string;
