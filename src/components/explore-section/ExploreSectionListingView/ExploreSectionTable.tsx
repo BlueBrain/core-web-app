974c76b40a6de643a5ae99c0a487505db714136c
Stefano Antonel <stefanoantonel@gmail.com>
2024-05-03 14:35:00 +0200

    Prettier

--- src/components/explore-section/ExploreSectionListingView/ExploreSectionTable.tsx
+++ src/components/explore-section/ExploreSectionListingView/ExploreSectionTable.tsx
@@ -239,7 +239,11 @@ export default function ExploreSectionTable({
   return (
     <>
       {enableDownload ? (
-        <WithRowSelection renderButton={renderButton ?? defaultRenderButton} dataType={dataType} selectionType={selectionType}>
+        <WithRowSelection
+          renderButton={renderButton ?? defaultRenderButton}
+          dataType={dataType}
+          selectionType={selectionType}
+        >
           {(rowSelection) => (
             <BaseTable
               columns={columns}
