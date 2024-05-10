e4fc2d2e4bc8e03bc85d6cc25882598dd51c2904
Konstantinos Platis <konstantinos.platis@epfl.ch>
2024-05-10 15:21:05 +0300

    Add supertitle

--- src/app/virtual-lab/sandbox/home/page.tsx
+++ src/app/virtual-lab/sandbox/home/page.tsx
@@ -50,6 +50,7 @@ export default function VirtualLabSandboxHomePage() {
         name="Welcome to Open Brain Platform"
         description="Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Aenean lacinia bibendum nulla sed consectetur. Donec id elit non mi porta gravida at eget metus. Vestibulum id ligula porta felis euismod semper. Maecenas faucibus mollis interdum."
         bottomElements={<SandboxStatistics />}
+        supertitle={null}
       />
       <VirtualLabCTABanner />
       <DiscoverObpPanel />
