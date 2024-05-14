b0839b961fb572bf9bc405d5c7087356984a42e2
Nick Smit <nick.smit@epfl.ch>
2024-05-14 15:22:25 +0200

    create lab button on the bottom

--- src/app/virtual-lab/lab/[virtualLabId]/(lab)/layout.tsx
+++ src/app/virtual-lab/lab/[virtualLabId]/(lab)/layout.tsx
@@ -7,7 +7,6 @@ import { LoadingOutlined } from '@ant-design/icons';
 import SimpleErrorComponent from '@/components/GenericErrorFallback';
 import VirtualLabTopMenu from '@/components/VirtualLab/VirtualLabTopMenu';
 import VirtualLabSidebar from '@/components/VirtualLab/VirtualLabSidebar';
-import { CreateVirtualLabButton } from '@/components/VirtualLab/VirtualLabTopMenu/CreateVirtualLabButton';
 
 export default function VirtualLabPageLayout({
   children,
@@ -16,7 +15,6 @@ export default function VirtualLabPageLayout({
   children: ReactNode;
   params: { virtualLabId: string };
 }) {
-  const extraHeaderItems: ReactNode[] = [<CreateVirtualLabButton key={1} />];
   return (
     <div className="inset-0 z-0 grid h-screen w-full grid-cols-[1fr_3fr] grid-rows-1 overflow-y-scroll bg-primary-9 p-10 text-white">
       <ErrorBoundary FallbackComponent={SimpleErrorComponent}>
@@ -29,7 +27,7 @@ export default function VirtualLabPageLayout({
       <ErrorBoundary FallbackComponent={SimpleErrorComponent}>
         <div className="ml-6">
           <Suspense fallback={<Spin indicator={<LoadingOutlined />} />}>
-            <VirtualLabTopMenu extraItems={extraHeaderItems} />
+            <VirtualLabTopMenu />
             {children}
           </Suspense>
         </div>
