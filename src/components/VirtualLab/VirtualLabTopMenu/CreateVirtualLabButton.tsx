1107fe0501b7b065b8a755d770b574fdbf64916c
Nick Smit <nick.smit@epfl.ch>
2024-05-14 17:32:30 +0200

    moved provider, modal to create button and removed references for provider, etc elsewhere

--- src/components/VirtualLab/VirtualLabTopMenu/CreateVirtualLabButton.tsx
+++ src/components/VirtualLab/VirtualLabTopMenu/CreateVirtualLabButton.tsx
@@ -1,18 +1,50 @@
-import { Button } from 'antd';
+import { Button, Modal } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
+import { VirtualLabCreateInformation, VirtualLabCreatePlan } from '../create';
+import { ModalStateProvider } from '@/components/VirtualLab/create/contexts/ModalStateContext';
 import { useModalState } from '@/components/VirtualLab/create/contexts/ModalStateContext';
+import { classNames } from '@/util/utils';
 
-export function CreateVirtualLabButton() {
-  const { showModal } = useModalState();
+type Props = {
+  extraClasses?: string;
+};
+
+export function CreateVirtualLabButton({ extraClasses = 'text-white' }: Props) {
+  return (
+    <ModalStateProvider>
+      <CreateVirtualLabButtonContent extraClasses={extraClasses} />
+    </ModalStateProvider>
+  );
+}
+
+function CreateVirtualLabButtonContent({ extraClasses }: Props) {
+  const { showModal, isModalVisible, currentStep, handleOk, handleCancel } = useModalState();
 
   return (
-    <Button
-      className="h-12 w-52 rounded-none border-none text-sm font-bold text-primary-8"
-      size="small"
-      onClick={showModal}
-    >
-      Create Virtual Lab
-      <PlusOutlined className="relative bottom-1 left-3" />
-    </Button>
+    <>
+      <Button
+        size="small"
+        className={classNames(
+          'h-full w-52 rounded-none border-none py-4 text-sm font-bold',
+          extraClasses
+        )}
+        type="link"
+        onClick={showModal}
+      >
+        Create Virtual Lab
+        <PlusOutlined className="relative bottom-1 left-3" />
+      </Button>
+      <Modal
+        title={null}
+        open={isModalVisible}
+        width={800}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer={null}
+      >
+        {currentStep === 'information' && <VirtualLabCreateInformation />}
+        {currentStep === 'plan' && <VirtualLabCreatePlan />}
+      </Modal>
+    </>
   );
 }
