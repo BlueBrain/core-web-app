f0763933f332c27f038ba249df62355eee310040
Nick Smit <nick.smit@epfl.ch>
2024-05-16 13:40:46 +0200

    rebased and added create vlab button improvements, integration into sandbox

--- src/components/VirtualLab/DiscoverObpPanel/index.tsx
+++ src/components/VirtualLab/DiscoverObpPanel/index.tsx
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import DiscoverLinks from './DiscoverLinks';
-=======
->>>>>>> 39549410 (Add virtual lab cta banner)
-=======
-import DiscoverLinks from './DiscoverLinks';
->>>>>>> 9d5a1187 (Modify discoverobp component)
 import DiscoverObpItem from './DiscoverObpItem';
 import { basePath } from '@/config';
 
@@ -13,15 +6,7 @@ export default function DiscoverObpPanel() {
   return (
     <div className="mt-10 flex flex-col gap-5">
       <div className="font-bold uppercase">Discover OBP</div>
-<<<<<<< HEAD
-<<<<<<< HEAD
       <div className="flex w-full flex-row gap-3">
-=======
-      <div className="flex flex-row gap-3">
->>>>>>> 39549410 (Add virtual lab cta banner)
-=======
-      <div className="flex w-full flex-row gap-3">
->>>>>>> 9d5a1187 (Modify discoverobp component)
         <DiscoverObpItem
           imagePath={`${basePath}/images/virtual-lab/obp_full_brain_blue.png`}
           title="Explore"
@@ -37,8 +22,6 @@ export default function DiscoverObpPanel() {
               </li>
             </ul>
           }
-<<<<<<< HEAD
-<<<<<<< HEAD
           bottomElement={
             <DiscoverLinks
               topLink="/virtual-lab/tutorials/explore"
@@ -47,20 +30,6 @@ export default function DiscoverObpPanel() {
               bottomText="Start exploring"
             />
           }
-=======
-          buttonText="Discover Explore"
-          buttonHref="/"
->>>>>>> 39549410 (Add virtual lab cta banner)
-=======
-          bottomElement={
-            <DiscoverLinks
-              topLink="/virtual-lab/tutorials/explore"
-              topText="How can I explore the brain?"
-              bottomLink="explore"
-              bottomText="Start exploring"
-            />
-          }
->>>>>>> 9d5a1187 (Modify discoverobp component)
         />
         <DiscoverObpItem
           imagePath={`${basePath}/images/virtual-lab/obp_vl_build.png`}
@@ -77,8 +46,6 @@ export default function DiscoverObpPanel() {
               </li>
             </ul>
           }
-<<<<<<< HEAD
-<<<<<<< HEAD
           bottomElement={
             <DiscoverLinks
               topLink="/virtual-lab/tutorials/build"
@@ -87,20 +54,6 @@ export default function DiscoverObpPanel() {
               bottomText="View models"
             />
           }
-=======
-          buttonText="Discover Models"
-          buttonHref="/"
->>>>>>> 39549410 (Add virtual lab cta banner)
-=======
-          bottomElement={
-            <DiscoverLinks
-              topLink="/virtual-lab/tutorials/build"
-              topText="How can I build models?"
-              bottomLink="build"
-              bottomText="View models"
-            />
-          }
->>>>>>> 9d5a1187 (Modify discoverobp component)
         />
         <DiscoverObpItem
           imagePath={`${basePath}/images/virtual-lab/obp_vl_simulate.png`}
@@ -117,8 +70,6 @@ export default function DiscoverObpPanel() {
               </li>
             </ul>
           }
-<<<<<<< HEAD
-<<<<<<< HEAD
           bottomElement={
             <DiscoverLinks
               topLink="/virtual-lab/tutorials/simulate"
@@ -127,20 +78,6 @@ export default function DiscoverObpPanel() {
               bottomText="View simulations"
             />
           }
-=======
-          buttonText="Discover Simulations"
-          buttonHref="/"
->>>>>>> 39549410 (Add virtual lab cta banner)
-=======
-          bottomElement={
-            <DiscoverLinks
-              topLink="/virtual-lab/tutorials/simulate"
-              topText="How can I launch simulations?"
-              bottomLink="simulate"
-              bottomText="View simulations"
-            />
-          }
->>>>>>> 9d5a1187 (Modify discoverobp component)
         />
       </div>
     </div>
