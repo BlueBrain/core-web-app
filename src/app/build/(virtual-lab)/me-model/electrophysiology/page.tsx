42b6bdfd8cbc0a99bfa78762b35a3c99c0ff5bf3
Stefano Maximiliano Antonel <stefano.antonel@epfl.ch>
2024-05-17 06:41:52 +0000

    Create E-Model card on summary

--- src/app/build/(virtual-lab)/me-model/electrophysiology/page.tsx
+++ src/app/build/(virtual-lab)/me-model/electrophysiology/page.tsx
@@ -2,16 +2,31 @@
 
 import { useSetAtom } from 'jotai';
 import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 import ExploreSectionListingView from '@/components/explore-section/ExploreSectionListingView';
 import { DataType } from '@/constants/explore-section/list-views';
-import { meModelSectionAtom } from '@/state/virtual-lab/build/me-model';
+import { selectedEModelAtom, meModelSectionAtom } from '@/state/virtual-lab/build/me-model';
 import MorphologyCard from '@/components/build-section/virtual-lab/me-model/MorphologyCard';
+import { Btn } from '@/components/Btn';
+import { ESeModel, ExploreESHit, ExploreResource } from '@/types/explore-section/es';
 
 export default function ElectrophysiologyPage() {
   const setMEModelSection = useSetAtom(meModelSectionAtom);
+  const setSelectedEModel = useSetAtom(selectedEModelAtom);
+  const router = useRouter();
 
   useEffect(() => setMEModelSection('electrophysiology'), [setMEModelSection]);
+
+  const onEModelPicked = (selectedRows: ExploreESHit<ExploreResource>[]) => {
+    if (selectedRows.length > 1) {
+      throw new Error('Multiple e-models selected for ME-Model building. Only one is allowed');
+    }
+
+    const emodel = selectedRows[0]._source as ESeModel;
+    setSelectedEModel(emodel);
+    router.push('/build/me-model/summary');
+  };
 
   return (
     <>
@@ -19,7 +34,20 @@ export default function ElectrophysiologyPage() {
         <MorphologyCard />
       </div>
       <div className="h-full px-10" id="explore-table-container-for-observable">
-        <ExploreSectionListingView dataType={DataType.CircuitEModel} brainRegionSource="selected" />
+        <ExploreSectionListingView
+          dataType={DataType.CircuitEModel}
+          brainRegionSource="selected"
+          enableDownload
+          selectionType="radio"
+          renderButton={({ selectedRows }) => (
+            <Btn
+              className="fit-content sticky bottom-0 ml-auto w-fit bg-secondary-2"
+              onClick={() => onEModelPicked(selectedRows)}
+            >
+              Validate
+            </Btn>
+          )}
+        />
       </div>
     </>
   );
