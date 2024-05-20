f75c9c116aa85710e66093036590901b9b432878
Bilal Meddah <bilal.meddah@epfl.ch>
2024-05-20 17:42:28 +0200

    update: add message for all kind of response from billing endpoints/stripe

---
+++ src/components/VirtualLab/Billing/messages.ts
@@ -0,0 +1,12 @@
+export const PAYMENT_SUCCEEDED =
+  "Your order has been placed. We'll send you an email with your order details.";
+export const PAYMENT_FAILED =
+  'We are sorry, there was an error processing your payment. Please try again with a different payment method';
+export const PROCESSING_TRANSACTION_FAILED =
+  "Oops! Something went wrong. Unable to add credit at the moment. Don't worry, we're on it!";
+export const FETCH_PAYMENT_METHODS_FAILED =
+  'Something went wrong when fetching virtual lab payment methods';
+export const ADD_NEW_PAYMENT_METHOD_DESCRIPTION =
+  'Enter your payment details below to save this card for paying for your virtual lab experiments. This will make future transactions quick and easy.';
+export const PREP_PAYMENT_FORM =
+  "We're having some trouble setting up your payment options at the moment. Please try again in a little while.";
