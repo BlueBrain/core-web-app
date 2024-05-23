d13a23b009763043b6fac7b408a3c97fb520de5e
Dinika Saxena <dinikasaxenas@gmail.com>
2024-05-23 12:39:36 +0200

    1663 // When session expires redirect to login
    Signed-off-by: Dinika Saxena <dinikasaxenas@gmail.com>

--- src/app/virtual-lab/layout.tsx
+++ src/app/virtual-lab/layout.tsx
@@ -2,9 +2,12 @@
 
 import { ErrorBoundary } from 'react-error-boundary';
 import { ReactNode } from 'react';
+import { useSession } from 'next-auth/react';
+
 import SimpleErrorComponent from '@/components/GenericErrorFallback';
 
 export default function VirtualLabLayout({ children }: { children: ReactNode }) {
+  useSession({ required: true });
   return (
     <div className="h-screen overflow-y-scroll bg-primary-9 text-white">
       <ErrorBoundary FallbackComponent={SimpleErrorComponent}>{children}</ErrorBoundary>
