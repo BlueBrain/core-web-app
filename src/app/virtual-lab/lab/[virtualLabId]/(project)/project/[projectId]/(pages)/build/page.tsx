f4d89bfa21f0120d1c6c8cdd654205f46df1e1f3
Harry Robert Taylor Anderson <handerso@bbd-9sczyl3.epfl.ch>
2024-05-24 19:30:35 +0200

    work in progress

--- src/app/virtual-lab/lab/[virtualLabId]/(project)/project/[projectId]/(pages)/build/page.tsx
+++ src/app/virtual-lab/lab/[virtualLabId]/(project)/project/[projectId]/(pages)/build/page.tsx
@@ -3,7 +3,10 @@
 import Image from 'next/image';
 import { createPortal } from 'react-dom';
 import { useAtom, useAtomValue } from 'jotai';
-import { Dispatch, useState, SetStateAction } from 'react';
+import { Dispatch, ReactNode, useRef, useState, SetStateAction } from 'react';
+import { Carousel } from 'antd';
+import { LeftOutlined, RightOutlined } from '@ant-design/icons';
+import { CarouselRef } from 'antd/lib/carousel';
 import { classNames } from '@/util/utils';
 import { basePath } from '@/config';
 import WithExploreEModel from '@/components/explore-section/EModel/WithExploreEModel';
@@ -19,37 +22,42 @@ enum SimulationScope {
 }
 
 function ScopeSelector({
+  children,
   selectedScope,
   setSelectedScope,
 }: {
+  children: ReactNode;
   selectedScope: SimulationScope | null;
   setSelectedScope: Dispatch<SetStateAction<SimulationScope | null>>;
 }) {
   const projectTopMenuRef = useAtomValue(projectTopMenuRefAtom);
 
   const controls = (
-    <>
-      <button
-        className={classNames('text-lg', !selectedScope && 'font-bold underline')}
-        onClick={() => setSelectedScope(null)}
-        type="button"
-      >
-        All
-      </button>
-      {(Object.values(SimulationScope) as Array<SimulationScope>).map((scope) => (
+    <div className="flex items-center justify-between gap-28">
+      <div className="flex gap-2">
         <button
-          key={scope}
-          className={classNames(
-            'text-lg capitalize',
-            selectedScope === scope && 'font-bold underline'
-          )}
-          onClick={() => setSelectedScope(scope)}
+          className={classNames('text-lg', !selectedScope && 'font-bold underline')}
+          onClick={() => setSelectedScope(null)}
           type="button"
         >
-          {scope}
+          All
         </button>
-      ))}
-    </>
+        {(Object.values(SimulationScope) as Array<SimulationScope>).map((scope) => (
+          <button
+            key={scope}
+            className={classNames(
+              'text-lg capitalize',
+              selectedScope === scope && 'font-bold underline'
+            )}
+            onClick={() => setSelectedScope(scope)}
+            type="button"
+          >
+            {scope}
+          </button>
+        ))}
+      </div>
+      <div className="flex items-center gap-4">{children}</div>
+    </div>
   );
   return projectTopMenuRef?.current && createPortal(controls, projectTopMenuRef.current);
 }
@@ -125,6 +133,55 @@ const items = [
   },
 ];
 
+function CustomSlide(props) {
+  const {
+    selectedSimulationScope,
+    setSelectedSimulationScope,
+    description,
+    key,
+    title,
+    src,
+    ...otherProps
+  } = props;
+
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <div {...otherProps}>
+      <button
+        className={classNames(
+          'flex h-[280px] w-[286px] flex-col gap-2 text-left text-white',
+          !!selectedSimulationScope &&
+            selectedSimulationScope !== key &&
+            'text-opacity-60 opacity-60 hover:text-opacity-100 hover:opacity-100',
+          !selectedSimulationScope && 'hover:font-bold'
+        )}
+        onClick={() => setSelectedSimulationScope(selectedSimulationScope !== key ? key : null)}
+        type="button"
+      >
+        <h2
+          className={classNames(
+            'text-3xl',
+            !!selectedSimulationScope && selectedSimulationScope === key && 'font-bold'
+          )}
+        >
+          {title}
+        </h2>
+        <span className="font-light">{description}</span>
+        <div className="relative w-full grow" style={{ opacity: 'inherit' }}>
+          <Image
+            fill
+            alt={`${title} Banner Image`}
+            style={{
+              objectFit: 'cover',
+            }}
+            src={src}
+          />
+        </div>
+      </button>
+    </div>
+  );
+}
+
 function ScopeCarousel() {
   const [selectedScope, setSelectedScope] = useState<SimulationScope | null>(null);
 
@@ -132,48 +189,48 @@ function ScopeCarousel() {
     selectedSimulationScopeAtom
   );
 
+  const carouselRef = useRef<CarouselRef>(null);
+  const progressRef = useRef(null);
+
+  // const [currentSlide, setCurrentSlide] = useState<number>(0);
+
+  const slidesToShow = 5;
+  const slidesToScroll = 3;
+
+  const carouselItems = items
+    .filter(({ scope }) => !selectedScope || scope === selectedScope)
+    .map(({ description, key, src, title }) => (
+      <CustomSlide
+        key={key}
+        description={description}
+        src={src}
+        title={title}
+        selectedSimulationScope={selectedSimulationScope}
+        setSelectedSimulationScope={setSelectedSimulationScope}
+      />
+    ));
+
   return (
     <>
-      <ScopeSelector selectedScope={selectedScope} setSelectedScope={setSelectedScope} />
-      <div className="flex w-full flex-nowrap gap-4 overflow-scroll">
-        {items
-          .filter(({ scope }) => !selectedScope || scope === selectedScope)
-          .map(({ description, key, src, title }) => (
-            <button
-              key={key}
-              className={classNames(
-                'flex min-w-[286px] flex-col gap-2 text-left text-white',
-                !!selectedSimulationScope &&
-                  selectedSimulationScope !== key &&
-                  'text-opacity-60 opacity-60 hover:text-opacity-100 hover:opacity-100',
-                !selectedSimulationScope && 'hover:font-bold'
-              )}
-              onClick={() =>
-                setSelectedSimulationScope(selectedSimulationScope !== key ? key : null)
-              }
-              type="button"
-            >
-              <h2
-                className={classNames(
-                  'text-3xl',
-                  !!selectedSimulationScope && selectedSimulationScope === key && 'font-bold'
-                )}
-              >
-                {title}
-              </h2>
-              <span className="font-light">{description}</span>
-              <div className="relative h-[122px] w-full" style={{ opacity: 'inherit' }}>
-                <Image
-                  fill
-                  alt={`${title} Banner Image`}
-                  style={{
-                    objectFit: 'cover',
-                  }}
-                  src={src}
-                />
-              </div>
-            </button>
-          ))}
+      <ScopeSelector selectedScope={selectedScope} setSelectedScope={setSelectedScope}>
+        <LeftOutlined onClick={() => carouselRef.current?.prev()}>Navigate scope left</LeftOutlined>
+        <div ref={progressRef} />
+        <RightOutlined onClick={() => carouselRef.current?.next()}>
+          Navigate scope right
+        </RightOutlined>
+      </ScopeSelector>
+      <div>
+        <Carousel
+          // afterChange={(current) => setCurrentSlide(current)}
+          ref={carouselRef}
+          arrows
+          dots
+          infinite={false}
+          slidesToShow={slidesToShow}
+          slidesToScroll={slidesToScroll}
+        >
+          {carouselItems}
+        </Carousel>
       </div>
     </>
   );
