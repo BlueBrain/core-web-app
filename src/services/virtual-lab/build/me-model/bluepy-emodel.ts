d43ea84fc0fcf48719667b747ed2e60059f9a8f3
Stefano Antonel <stefanoantonel@gmail.com>
2024-05-28 11:05:12 +0200

    Launch me-model-analysis with on-demand-svc
    - Create a common WebSocket file to be reused
    - Add AWS endpoint for me-model-analysis
    - When user clicks on Validate, we launch the task in the background to launch the me-model-analysis
    
    Closes BBPP134-1701

---
+++ src/services/virtual-lab/build/me-model/bluepy-emodel.ts
@@ -0,0 +1,41 @@
+import Ws, { BluePyEModelCmd, WSResponses } from './websocket';
+
+import { meModelAnalysisSvc } from '@/config';
+
+interface BluePyEModelConfig {
+  onInit?: () => void;
+  onAnalysisDone?: () => void;
+}
+
+export default class BluePyEModelCls {
+  private config: BluePyEModelConfig;
+
+  private ws: Ws;
+
+  constructor(modelId: string, token: string, config: BluePyEModelConfig = {}) {
+    this.config = config;
+    this.ws = new Ws(meModelAnalysisSvc.wsUrl, token, this.onMessage);
+    this.ws.send(BluePyEModelCmd.SET_MODEL, { model_id: modelId });
+  }
+
+  runAnalysis() {
+    this.ws.send(BluePyEModelCmd.RUN_ANALYSIS, {});
+  }
+
+  private onMessage = (cmd: WSResponses) => {
+    switch (cmd) {
+      case 'set_model_done':
+        this.config.onInit?.();
+        break;
+      case 'run_analysis_done':
+        this.config.onAnalysisDone?.();
+        break;
+      default:
+        break;
+    }
+  };
+
+  destroy() {
+    this.ws.destroy();
+  }
+}
