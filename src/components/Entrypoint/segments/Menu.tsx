0f850cdb32e30a59919ff0de47a6b19373a4a63a
Nick Smit <nick.smit@epfl.ch>
2024-05-29 14:29:29 +0200

    changed nav buttons

--- src/components/Entrypoint/segments/Menu.tsx
+++ src/components/Entrypoint/segments/Menu.tsx
@@ -10,7 +10,6 @@ import { basePath } from '@/config';
 
 interface BaseEntrypointMenuItem {
   title: string;
-  description: string;
   bgcolor: string;
 }
 interface EntrypointMenuItemAsButton extends BaseEntrypointMenuItem {
@@ -24,35 +23,26 @@ type EntrypointMenuItem = EntrypointMenuItemAsButton | EntrypointMenuItemAsLink;
 const ENTRYPOINT_MENU_ITEMS: Array<EntrypointMenuItem> = [
   {
     title: 'Explore',
-    description: 'Explore brain models and experimental data',
     href: '/explore/interactive',
-    bgcolor: 'bg-primary-5',
+    bgcolor: 'bg-primary-8',
   },
   {
     title: 'About',
-    description: 'Documentation, release notes, roadmap',
     href: '/about',
-    bgcolor: 'bg-primary-6',
+    bgcolor: 'bg-primary-8',
   },
   {
     title: 'Log in',
-    description: 'Get access to all your models & experiments',
-    bgcolor: 'bg-primary-7',
+    bgcolor: 'bg-primary-8',
     action: (callbackUrl: string) =>
       signIn('keycloak', { callbackUrl: callbackUrl ?? `${basePath}/main` }),
-  },
-  {
-    title: 'Sign up',
-    href: '/sign-up',
-    description: 'Create an account and start experimenting with BBOP',
-    bgcolor: 'bg-primary-8',
   },
 ];
 
 function withButtonOrLink(WrapperComponent: ComponentType<EntrypointMenuItem>) {
   // eslint-disable-next-line react/display-name
-  return function Action({ bgcolor, description, title, ...rest }: EntrypointMenuItem) {
-    const key = kebabCase(`${title}-${description}`);
+  return function Action({ bgcolor, title, ...rest }: EntrypointMenuItem) {
+    const key = kebabCase(`${title}`);
     const params = useSearchParams();
     if ('action' in rest && rest.action) {
       const origin = params?.get('origin');
@@ -60,43 +50,38 @@ function withButtonOrLink(WrapperComponent: ComponentType<EntrypointMenuItem>) {
       const onClick = () => () => rest.action?.(callbackUrl);
       return (
         <button key={key} type="button" aria-label={title} onClick={onClick()}>
-          <WrapperComponent {...{ bgcolor, description, title }} />
+          <WrapperComponent {...{ bgcolor, title }} />
         </button>
       );
     }
     if ('href' in rest) {
       return (
         <Link key={key} href={rest.href}>
-          <WrapperComponent {...{ bgcolor, description, title }} />
+          <WrapperComponent {...{ bgcolor, title }} />
         </Link>
       );
     }
     return null;
   };
 }
-
-function EntrypointMenuSingleItem({
-  title,
-  description,
-  bgcolor,
-}: {
-  title: string;
-  description: string;
-  bgcolor: string;
-}) {
+function EntrypointMenuSingleItem({ title, bgcolor }: { title: string; bgcolor: string }) {
   return (
-    <div className={classNames('box-border h-full cursor-pointer p-4 hover:bg-primary-4', bgcolor)}>
-      <h3 className="mb-1 text-left text-2xl font-bold text-white">{title}</h3>
-      <p className="line-clamp-2 text-left text-sm font-normal text-primary-1">{description}</p>
+    <div
+      className={classNames(
+        'box-border h-auto w-[168px] cursor-pointer py-4 hover:bg-primary-4',
+        bgcolor
+      )}
+    >
+      <h3 className="my-auto pl-8 text-left text-xl font-semibold text-white">{title}</h3>
     </div>
   );
 }
 
 export default function EntrypointMenu() {
   return (
-    <div className="absolute left-7 right-7 top-7 z-20 mx-auto  grid w-[calc(100%-3.25rem)] grid-cols-[1fr_4fr]">
+    <div className="absolute left-7 right-7 top-7 z-20 mx-auto grid w-[calc(100%-3.25rem)] grid-cols-[1fr_3fr]">
       <OBPLogo />
-      <div className="grid grid-cols-4 gap-1">
+      <div className="absolute right-7 top-7 flex justify-between gap-1">
         {ENTRYPOINT_MENU_ITEMS.map((props) => withButtonOrLink(EntrypointMenuSingleItem)(props))}
       </div>
     </div>
