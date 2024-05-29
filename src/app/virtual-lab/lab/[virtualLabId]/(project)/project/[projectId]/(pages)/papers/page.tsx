c5434581da5f086f0fb2d9e8cf3d88b091e80c5c
Bilal Meddah <bilal.meddah@epfl.ch>
2024-05-29 12:18:51 +0200

    update: refactor papers listing view -use server component-

--- src/app/virtual-lab/lab/[virtualLabId]/(project)/project/[projectId]/(pages)/papers/page.tsx
+++ src/app/virtual-lab/lab/[virtualLabId]/(project)/project/[projectId]/(pages)/papers/page.tsx
@@ -1,16 +1,36 @@
+import Link from 'next/link';
+
+import { auth } from '@/auth';
 import PaperListView from '@/components/papers/PaperListView';
 import { ServerSideComponentProp } from '@/types/common';
+import retrievePapersList from '@/services/paper-ai/retrievePapersList';
 
-function PaperListPage({
-  params,
+
+
+export default async function PapersListing({
+  params: { virtualLabId, projectId },
 }: ServerSideComponentProp<{ virtualLabId: string; projectId: string }>) {
-  const { virtualLabId, projectId } = params;
+  const session = await auth();
+  if (!session) return;
+
+  const { papers, total } = await retrievePapersList({
+    virtualLabId, projectId,
+    accessToken: session.accessToken,
+  })
 
   return (
-    <div className="mt-4 h-[80vh] bg-white p-8 text-slate-800">
-      <PaperListView virtualLabId={virtualLabId} projectId={projectId} />
+    <div
+      id="project-papers-container"
+      className="mt-4 max-h-[80vh] h-full bg-white p-8 flex flex-col relative"
+    >
+      <PaperListView {...{ total, papers }} />
+      <Link
+        className="bg-primary-8 px-8 py-4 flex items-center justify-center text-white max-w-max self-end mt-auto"
+        type='primary'
+        href={`/virtual-lab/lab/${virtualLabId}/project/${projectId}/papers/create`}
+      >
+        Create new paper
+      </Link>
     </div>
   );
 }
-
-export default PaperListPage;
