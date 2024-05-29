afc612f15425055f2a8a220510d279df55d7a09d
Nick Smit <nick.smit@epfl.ch>
2024-05-29 14:48:35 +0200

    changed hero text

--- src/components/Entrypoint/segments/Splash.tsx
+++ src/components/Entrypoint/segments/Splash.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
+import { Button } from 'antd';
+import { ArrowRightOutlined } from '@ant-design/icons';
 import { basePath } from '@/config';
 import { classNames } from '@/util/utils';
 
@@ -19,7 +21,13 @@ export function Background() {
 function HeroText() {
   return (
     <div className="absolute top-1/2 z-20 -translate-y-1/2 items-center justify-center lg:left-10percent">
-      <div className="select-none whitespace-pre-line text-left text-4xl font-bold text-white xl:text-7xl 2xl:text-9xl">{`Leveraging\nsimulation\nneuroscience `}</div>
+      <div className="select-none whitespace-pre-line text-left text-2xl font-bold text-white xl:text-7xl 2xl:text-9xl">
+        {'Virtual labs for\nexploring, building and\nsimulating the brain'}
+      </div>
+      <Button className="mt-10 flex h-auto w-[500px] justify-between rounded-none border-solid border-primary-7 bg-transparent py-8 text-sm font-bold">
+        <span className="pl-4 text-4xl text-white">Start exploring</span>
+        <ArrowRightOutlined className="pr-4 text-4xl text-white" />
+      </Button>
     </div>
   );
 }
