6ba75c0ce118df948949e9ca8013cf3eb7ef1c1f
Stefano Antonel <stefanoantonel@gmail.com>
2024-05-31 11:56:41 +0200

    Pick ME-Model to Simulate
    Closes BBPP134-1743

--- src/app/virtual-lab/lab/[virtualLabId]/(project)/project/[projectId]/(pages)/simulate/page.tsx
+++ src/app/virtual-lab/lab/[virtualLabId]/(project)/project/[projectId]/(pages)/simulate/page.tsx
@@ -1,3 +1,32 @@
+'use client';
+
+import ExploreSectionListingView from '@/components/explore-section/ExploreSectionListingView';
+import { DataType } from '@/constants/explore-section/list-views';
+import { Btn } from '@/components/Btn';
+import GenericButton from '@/components/Global/GenericButton';
+import Link from '@/components/Link';
+
 export default function VirtualLabProjectSimulatePage() {
-  return <div>Simulate</div>;
+  return (
+    <div className="flex flex-col pt-14">
+      <div className="flex">
+        <GenericButton text="Simulations" className="bg-white text-2xl font-bold text-primary-8" />
+        <div className="grow" />
+        <Link href="simulate/new">
+          <GenericButton text="New simulation ＋" className="bg-primary-6 text-white" />
+        </Link>
+      </div>
+      <ExploreSectionListingView
+        dataType={DataType.SimulationCampaigns}
+        brainRegionSource="selected"
+        selectionType="radio"
+        enableDownload
+        renderButton={() => (
+          <Btn className="fit-content sticky bottom-0 ml-auto w-fit bg-secondary-2">
+            Use ME-Model
+          </Btn>
+        )}
+      />
+    </div>
+  );
 }
