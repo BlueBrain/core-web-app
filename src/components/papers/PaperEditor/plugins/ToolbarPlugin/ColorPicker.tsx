4adf88b800b960ebf2d6ce717a662e36acb282da
Bilal Meddah <bilal.meddah@epfl.ch>
2024-05-31 23:15:39 +0200

    update: improce action, toolbar plugins
    update: editor theme with new heading (4, 5, 6)

--- src/components/papers/PaperEditor/plugins/ToolbarPlugin/ColorPicker.tsx
+++ src/components/papers/PaperEditor/plugins/ToolbarPlugin/ColorPicker.tsx
@@ -1,25 +1,66 @@
+import { useCallback } from 'react';
 import { ColorPicker } from 'antd';
 import { Color } from 'antd/es/color-picker';
+import { $getSelection, LexicalEditor } from 'lexical';
+import { $patchStyleText } from '@lexical/selection';
+
 import EditorButton from '../../molecules/Button';
 import { FormatColorBg, FormatColorText } from '@/components/icons/EditorIcons';
 
 type Props = {
+  editor: LexicalEditor;
   title: string;
   disabled?: boolean;
   color: Color;
-  onChange?: (color: Color) => void;
   'aria-label'?: string;
   type: 'bg' | 'text';
 };
 
 export default function EditorColorPicker({
+  editor,
   title,
   disabled = false,
   color,
-  onChange,
   type,
   'aria-label': arialLabel,
 }: Props) {
+  const applyStyle = useCallback(
+    (styles: Record<string, string>, skipHistoryStack?: boolean) => {
+      editor.update(
+        () => {
+          const selection = $getSelection();
+          if (selection !== null) {
+            $patchStyleText(selection, styles);
+          }
+        },
+        skipHistoryStack ? { tag: 'historic' } : {}
+      );
+    },
+    [editor]
+  );
+
+  const onFontColorSelect = useCallback(
+    (value: Color) => {
+      applyStyle({ color: `#${value.toHex()}` });
+    },
+    [applyStyle]
+  );
+
+  const onBgColorSelect = useCallback(
+    (value: Color) => {
+      applyStyle({ 'background-color': `#${value.toHex()}` });
+    },
+    [applyStyle]
+  );
+
+  const onChange = (value: Color) => {
+    if (type === 'text') {
+      onFontColorSelect(value);
+    } else if (type === 'bg') {
+      onBgColorSelect(value);
+    }
+  };
+
   return (
     <ColorPicker
       trigger="click"
