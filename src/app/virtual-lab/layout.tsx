a3b2c24ac3a4a528c1f036823f037524293a46cd
Bilal Meddah <bilal.meddah@epfl.ch>
2024-05-31 23:19:28 +0200

    update: refactor paper pages

--- src/app/virtual-lab/layout.tsx
+++ src/app/virtual-lab/layout.tsx
@@ -9,7 +9,7 @@ import SimpleErrorComponent from '@/components/GenericErrorFallback';
 export default function VirtualLabLayout({ children }: { children: ReactNode }) {
   useSession({ required: true });
   return (
-    <div className="h-screen overflow-y-scroll bg-primary-9 text-white">
+    <div className="h-screen overflow-y-auto bg-primary-9 text-white">
       <ErrorBoundary FallbackComponent={SimpleErrorComponent}>{children}</ErrorBoundary>
     </div>
   );
