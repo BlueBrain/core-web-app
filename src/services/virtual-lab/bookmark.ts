866590dc8180ba95ca334b8eb9c49f032c8d31b6
Dinika Saxena <dinikasaxenas@gmail.com>
2024-06-03 10:31:13 +0200

    Use fetch API instead of localstorage API

--- src/services/virtual-lab/bookmark.ts
+++ src/services/virtual-lab/bookmark.ts
@@ -1,54 +1,65 @@
+import { createVLApiHeaders } from './common';
 import { DataType } from '@/constants/explore-section/list-views';
+import { virtualLabApi } from '@/config';
 import { Bookmark, BookmarksByCategory } from '@/types/virtual-lab/bookmark';
 
-export async function addBookmark(lab: string, labProject: string, bookmark: Bookmark) {
-  return getBookmarkedItems(lab, labProject).then((items) => {
-    localStorage.setItem(keyFor(lab, labProject), JSON.stringify(items.concat(bookmark)));
-  });
-}
-
-export async function removeBookmark(lab: string, labProject: string, bookmark: Bookmark) {
-  return getBookmarkedItems(lab, labProject).then((items) => {
-    localStorage.setItem(
-      keyFor(lab, labProject),
-      JSON.stringify(
-        items.filter(
-          (i) => i.resourceId !== bookmark.resourceId && i.category !== bookmark.category
-        )
-      )
-    );
-  });
-}
-
-const keyFor = (lab: string, project: string): string => `bookmarkedItems-${lab}-${project}`;
-
-async function getBookmarkedItems(lab: string, labProject: string): Promise<Bookmark[]> {
-  const stringValue = localStorage.getItem(keyFor(lab, labProject));
-
-  return Promise.resolve(stringValue ? (JSON.parse(stringValue) as Bookmark[]) : []);
-}
-
-// Temporary function that returns bookmarks by experiment categories. This will soon be replaced by a server call.
-export async function getBookmarksByCategory(
+export async function addBookmark(
   lab: string,
-  labProject: string
-): Promise<BookmarksByCategory> {
-  const stringValue = localStorage.getItem(keyFor(lab, labProject));
-
-  if (stringValue) {
-    const bookmarks = JSON.parse(stringValue) as Bookmark[];
-    const bookmarksByCategory = bookmarks.reduce((acc, curr) => {
-      if (curr.category in acc) {
-        return { ...acc, [curr.category]: [...acc[curr.category], curr] };
-      }
-
-      return { ...acc, [curr.category]: [curr] };
-    }, {} as BookmarksByCategory);
-
-    return Promise.resolve(bookmarksByCategory);
+  labProject: string,
+  bookmark: Bookmark,
+  token: string = ''
+) {
+  const response = await fetch(
+    `${virtualLabApi.url}/virtual-labs/${lab}/projects/${labProject}/bookmarks`,
+    {
+      method: 'POST',
+      headers: createVLApiHeaders(token),
+      body: JSON.stringify(bookmark),
+    }
+  );
+  if (!response.ok) {
+    throw new Error(`Status: ${response.status}`);
   }
 
-  return Promise.resolve({} as BookmarksByCategory);
+  return response.json();
+}
+
+export async function removeBookmark(
+  lab: string,
+  labProject: string,
+  bookmark: Bookmark,
+  token: string = ''
+) {
+  const response = await fetch(
+    `${virtualLabApi.url}/virtual-labs/${lab}/projects/${labProject}/bookmarks/${bookmark.resourceId}?category=${bookmark.category}`,
+    {
+      method: 'DELETE',
+      headers: createVLApiHeaders(token),
+    }
+  );
+  if (!response.ok) {
+    throw new Error(`Status: ${response.status}`);
+  }
+
+  return response.json();
+}
+
+export async function getBookmarksByCategory(
+  lab: string,
+  labProject: string,
+  token: string = ''
+): Promise<BookmarksByCategory> {
+  const response = await fetch(
+    `${virtualLabApi.url}/virtual-labs/${lab}/projects/${labProject}/bookmarks`,
+    {
+      method: 'GET',
+      headers: createVLApiHeaders(token),
+    }
+  );
+  if (!response.ok) {
+    throw new Error(`Status: ${response.status}`);
+  }
+  return response.json();
 }
 
 export const getBookmarksCount = (bookmarksByCategory: BookmarksByCategory): number => {
