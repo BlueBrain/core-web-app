f8514c2970e5075aa9609c5ff965ab733377abb0
Harry Robert Taylor Anderson <handerso@bbd-9sczyl3.epfl.ch>
2024-06-03 10:08:22 +0200

    add bbop logo

--- src/app/log-in/page.tsx
+++ src/app/log-in/page.tsx
@@ -4,37 +4,41 @@ import { signIn } from 'next-auth/react';
 import { Button, ConfigProvider } from 'antd';
 import { GitlabFilled, GithubOutlined, GoogleOutlined } from '@ant-design/icons';
 
+import { OBPLogo } from '@/components/Entrypoint/segments/Splash';
 import { basePath } from '@/config';
 
 export default function Page() {
   return (
-    <div className="flex flex-col gap-8">
-      <div className="text-2xl font-bold text-white">Login to your virtual lab</div>
-      <ConfigProvider
-        theme={{
-          components: {
-            Button: {
-              colorPrimaryHover: '#1890FF',
-              contentFontSize: 16,
+    <>
+      <OBPLogo className="absolute left-10 top-10" color="text-white" />
+      <div className="flex flex-col gap-8">
+        <div className="text-2xl font-bold text-white">Login to your virtual lab</div>
+        <ConfigProvider
+          theme={{
+            components: {
+              Button: {
+                colorPrimaryHover: '#1890FF',
+                contentFontSize: 16,
+              },
             },
-          },
-        }}
-      >
-        <div className="flex items-center gap-8">
-          <div className="flex h-[124px] w-[124px] items-center justify-center rounded-full bg-white">
-            <GoogleOutlined className="text-6xl" />
+          }}
+        >
+          <div className="flex items-center gap-8">
+            <div className="flex h-[124px] w-[124px] items-center justify-center rounded-full bg-white">
+              <GoogleOutlined className="text-6xl" />
+            </div>
+            <Button
+              className="rounded-full"
+              onClick={() => signIn('keycloak', { callbackUrl: `${basePath}/virtual-lab` })}
+              icon={<GithubOutlined style={{ fontSize: 54 }} />}
+              style={{ width: 124, height: 124 }}
+            />
+            <div className="flex h-[124px] w-[124px] items-center justify-center rounded-full bg-white">
+              <GitlabFilled className="text-6xl" />
+            </div>
           </div>
-          <Button
-            className="rounded-full"
-            onClick={() => signIn('keycloak', { callbackUrl: `${basePath}/virtual-lab` })}
-            icon={<GithubOutlined style={{ fontSize: 54 }} />}
-            style={{ width: 124, height: 124 }}
-          />
-          <div className="flex h-[124px] w-[124px] items-center justify-center rounded-full bg-white">
-            <GitlabFilled className="text-6xl" />
-          </div>
-        </div>
-      </ConfigProvider>
-    </div>
+        </ConfigProvider>
+      </div>
+    </>
   );
 }
