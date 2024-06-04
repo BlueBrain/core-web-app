195d7be93d7e06f33cc70ebc6093933a22edfd62
Gil Arturo Barrios Del Villar <gil.barriosdelvillar@epfl.ch>
2024-06-04 16:48:16 +0200

    improve authenticated routs

--- src/app/virtual-lab/(home)/layout.tsx
+++ src/app/virtual-lab/(home)/layout.tsx
@@ -6,13 +6,8 @@ import { ErrorBoundary } from 'react-error-boundary';
 import SimpleErrorComponent from '@/components/GenericErrorFallback';
 import VirtualLabTopMenu from '@/components/VirtualLab/VirtualLabTopMenu';
 import { OBPLogo } from '@/components/Entrypoint/segments/Splash';
-import { useAuthenticatedRoute } from '@/hooks/server-safe-hooks';
 
 export default function VirtualLabPageLayout({ children }: { children: ReactNode }) {
-  const session = useAuthenticatedRoute();
-  if (!session) return null; // Avoids pre-rendering the content during ssr
-
-  conso
   return (
     <div className="bg-primary-9 p-10 text-white">
       <div className="flex flex-row justify-between">
--- src/app/virtual-lab/(home)/page.tsx
+++ src/app/virtual-lab/(home)/page.tsx
@@ -5,7 +5,7 @@ import { getVirtualLabsOfUser } from '@/services/virtual-lab/labs';
 import { useAuthenticatedRoute } from '@/hooks/server-safe-hooks';
 
 export default async function VirtualLabMainPage() {
-  await useAuthenticatedRoute()
+  await useAuthenticatedRoute();
   const virtualLabs = await getVirtualLabsOfUser();
   if (virtualLabs.data.results.length === 0) {
     redirect('/virtual-lab/sandbox/home');
