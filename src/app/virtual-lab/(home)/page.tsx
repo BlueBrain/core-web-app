8b5e4b31446b606e5973b1d5f98ffa7213de3bdf
Gil Arturo Barrios Del Villar <gil.barriosdelvillar@epfl.ch>
2024-06-04 20:41:37 +0200

    remove unnecessary protected routs

--- src/app/virtual-lab/(home)/page.tsx
+++ src/app/virtual-lab/(home)/page.tsx
@@ -2,10 +2,8 @@ import { redirect } from 'next/navigation';
 
 import VirtualLabDashboard from '@/components/VirtualLab/VirtualLabDashboard';
 import { getVirtualLabsOfUser } from '@/services/virtual-lab/labs';
-import { useAuthenticatedRoute } from '@/hooks/server-safe-hooks';
 
 export default async function VirtualLabMainPage() {
-  await useAuthenticatedRoute();
   const virtualLabs = await getVirtualLabsOfUser();
   if (virtualLabs.data.results.length === 0) {
     redirect('/virtual-lab/sandbox/home');
--- src/app/virtual-lab/layout.tsx
+++ src/app/virtual-lab/layout.tsx
@@ -1,10 +1,8 @@
 import { ErrorBoundary } from 'react-error-boundary';
 import { ReactNode } from 'react';
 import SimpleErrorComponent from '@/components/GenericErrorFallback';
-import { useAuthenticatedRoute } from '@/hooks/server-safe-hooks';
 
 export default async function VirtualLabLayout({ children }: { children: ReactNode }) {
-  await useAuthenticatedRoute();
   return (
     <div className="h-screen overflow-y-auto bg-primary-9 text-white">
       <ErrorBoundary FallbackComponent={SimpleErrorComponent}>{children}</ErrorBoundary>
