1f78e6408ac9bac1d2208c07c90df15ea3b67489
Gil Arturo Barrios Del Villar <gil.barriosdelvillar@epfl.ch>
2024-06-20 11:35:21 +0000

    Fix login page path, images not showing when deploying to a subpath

--- src/auth.ts
+++ src/auth.ts
@@ -6,6 +6,7 @@ import { env } from '@/env.mjs';
 const issuer = env.KEYCLOAK_ISSUER;
 const clientId = env.KEYCLOAK_CLIENT_ID;
 const clientSecret = env.KEYCLOAK_CLIENT_SECRET;
+const basePath = env.NEXT_PUBLIC_BASE_PATH;
 
 /**
  * Takes a token, and returns a new token with updated
@@ -125,7 +126,7 @@ export const authOptions: NextAuthOptions = {
     maxAge: 1800,
   },
   pages: {
-    signIn: '/log-in',
+    signIn: basePath + '/log-in', // eslint-disable-line
   },
 } satisfies NextAuthOptions;
 
