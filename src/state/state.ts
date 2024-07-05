ec4387672c28d2d02b349627c203e2da632d55a5
Gil Arturo Barrios Del Villar <gil.barriosdelvillar@epfl.ch>
2024-07-05 20:21:42 +0200

    state

--- src/state/state.ts
+++ src/state/state.ts
@@ -3,7 +3,6 @@ import { WritableAtom, Atom, atom, useAtomValue } from 'jotai';
 const atoms: Map<string, Atom<any>> = new Map();
 
 export function getAtom<T>(key: string, defaultValue: T | null = null) {
-  console.log(key, defaultValue, atoms.get(key))
   const theAtom = atoms.get(key) ?? atom(defaultValue);
   atoms.set(key, theAtom);
   return theAtom as WritableAtom<T | null, [T | null], void>;
