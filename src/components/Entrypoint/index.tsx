6e4529efd9b275208bd5e9da9bcc4b151a676219
Harry Robert Taylor Anderson <harry.anderson@epfl.ch>
2024-07-08 10:07:01 +0000

    Temporarily remove Release Notes

--- src/components/Entrypoint/index.tsx
+++ src/components/Entrypoint/index.tsx
@@ -1,13 +1,12 @@
 'use client';
 
-import { Splash, Menu, ReleaseNotes, AcceptInviteErrorDialog } from './segments';
+import { Splash, Menu, AcceptInviteErrorDialog } from './segments';
 
 export default function Entrypoint({ errorCode }: { errorCode?: string }) {
   return (
     <div className="relative flex h-screen max-h-screen w-screen flex-col p-5">
       <Splash />
       <Menu />
-      <ReleaseNotes />
       {errorCode && <AcceptInviteErrorDialog errorCode={errorCode} />}
     </div>
   );
