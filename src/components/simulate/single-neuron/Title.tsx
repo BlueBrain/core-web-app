67aa0878266ea0e1ec314fbeeed349d12965f0e6
Nicolaas Smit <nick.smit@epfl.ch>
2024-07-25 08:23:29 +0000

    Style and information update for simulate form

--- src/components/simulate/single-neuron/Title.tsx
+++ src/components/simulate/single-neuron/Title.tsx
@@ -1,10 +1,7 @@
 export default function Title() {
   return (
     <div className="flex h-[36px] items-center justify-between">
-      <span className="flex-grow uppercase text-neutral-4">Simulation experiment setup</span>
-
-      <span className="text-neutral-3">Creation date:</span>
-      <span className="text-primary-8">12.01.2024</span>
+      <span className="flex-grow pl-2 uppercase text-neutral-4">Simulation experiment setup</span>
     </div>
   );
 }
