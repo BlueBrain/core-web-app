090c995a4a063bda962935273b5703b819588c8e
Bilal Meddah <bilal.meddah@epfl.ch>
2024-07-30 13:51:13 +0000

    Place synapses in 3d morpho

--- src/components/build-section/virtual-lab/synaptome/NeuronModelView.tsx
+++ src/components/build-section/virtual-lab/synaptome/NeuronModelView.tsx
@@ -1,60 +1,106 @@
 import { useCallback, useEffect, useRef } from 'react';
+
+import {
+  DISPLAY_SYNAPSES_3D_EVENT,
+  REMOVE_SYNAPSES_3D_EVENT,
+  DisplaySynapses3DEvent,
+  RemoveSynapses3DEvent,
+} from './events';
 import Renderer from '@/services/bluenaas-single-cell/renderer';
 import { Morphology } from '@/services/bluenaas-single-cell/types';
-import { getSession } from '@/authFetch';
-import { blueNaasUrl } from '@/config';
+import getMorphology from '@/services/bluenaas-synaptome/getMorphology';
 
 type Props = {
   modelSelfUrl: string;
 };
 
+function useMorphology({
+  modelSelfUrl,
+  callback,
+}: {
+  modelSelfUrl: string;
+  callback: (morphology: Morphology) => void;
+}) {
+  const readMorphology = useCallback(async (): Promise<Morphology> => {
+    const response = await getMorphology({ modelId: modelSelfUrl });
+    const reader = response.body?.getReader();
+    let data: string = '';
+    let value: Uint8Array | undefined;
+    let done: boolean = false;
+    const decoder = new TextDecoder();
+    if (reader) {
+      while (!done) {
+        ({ done, value } = await reader.read());
+        const decodedChunk = decoder.decode(value, { stream: true });
+        data += decodedChunk;
+      }
+    }
+    return JSON.parse(data);
+  }, [modelSelfUrl]);
+
+  useEffect(() => {
+    (async function start() {
+      const morphology = await readMorphology();
+      callback(morphology);
+    })();
+  }, [callback, readMorphology]);
+}
+
 export default function NeuronModelView({ modelSelfUrl }: Props) {
   const containerRef = useRef<HTMLDivElement>(null);
   const rendererRef = useRef<Renderer | null>(null);
 
-  const onLoad = useCallback((morphology: Morphology) => {
-    if (rendererRef.current) {
-      const prunedMorph = rendererRef.current.removeNoDiameterSection(morphology);
-      rendererRef.current.addMorphology(prunedMorph);
+  const runRenderer = useCallback((morphology: Morphology) => {
+    if (!rendererRef.current && containerRef.current) {
+      const renderer = new Renderer(containerRef.current, {});
+      rendererRef.current = renderer;
+      const prunedMorph = renderer.removeNoDiameterSection(morphology);
+      renderer.addMorphology(prunedMorph);
     }
   }, []);
 
   useEffect(() => {
-    async function readMorphology(): Promise<Morphology> {
-      const session = await getSession();
-      const response = await fetch(
-        `${blueNaasUrl}/morphology?model_id=${encodeURIComponent(modelSelfUrl)}`,
-        {
-          method: 'get',
-          headers: {
-            accept: 'application/x-ndjson',
-            authorization: `bearer ${session.accessToken}`,
-          },
-        }
-      );
-      const reader = response.body?.getReader();
-      let data: string = '';
-      let value: Uint8Array | undefined;
-      let done: boolean = false;
-      const decoder = new TextDecoder();
-      if (reader) {
-        while (!done) {
-          ({ done, value } = await reader.read());
-          const decodedChunk = decoder.decode(value, { stream: true });
-          data += decodedChunk;
-        }
+    function displaySynapses3DEventHandler(event: DisplaySynapses3DEvent) {
+      if (rendererRef.current) {
+        const { objects } = event.detail;
+        rendererRef.current.addSynapses(objects);
       }
-      return JSON.parse(data);
     }
 
-    if (!rendererRef.current && containerRef.current) {
-      rendererRef.current = new Renderer(containerRef.current, {});
-      (async () => {
-        const morphology = await readMorphology();
-        onLoad(morphology);
-      })();
+    function removeSynapses3DEventHandler(event: RemoveSynapses3DEvent) {
+      if (rendererRef.current) {
+        const { objects } = event.detail;
+        if (objects) {
+          rendererRef.current.removeSynapses(objects);
+        }
+      }
     }
-  }, [modelSelfUrl, onLoad]);
+
+    window.addEventListener(
+      DISPLAY_SYNAPSES_3D_EVENT,
+      displaySynapses3DEventHandler as EventListener
+    );
+    window.addEventListener(
+      REMOVE_SYNAPSES_3D_EVENT,
+      removeSynapses3DEventHandler as EventListener
+    );
+
+    return () => {
+      window.removeEventListener(
+        DISPLAY_SYNAPSES_3D_EVENT,
+        displaySynapses3DEventHandler as EventListener
+      );
+      window.removeEventListener(
+        REMOVE_SYNAPSES_3D_EVENT,
+        removeSynapses3DEventHandler as EventListener
+      );
+    };
+  }, []);
+
+  useMorphology({
+    modelSelfUrl,
+    callback: runRenderer,
+  });
 
   return (
     <div className="relative h-full w-full">
--- src/util/utils.ts
+++ src/util/utils.ts
@@ -268,3 +268,9 @@ export function assertErrorMessage(e: any) {
   if (e instanceof Error) return e.message;
   return 'Something went wrong...';
 }
+
+export function getRandomIntInclusive(min: number, max: number) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}
