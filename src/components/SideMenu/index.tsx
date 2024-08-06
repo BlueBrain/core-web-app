868d9fdb760502b2086d26798806d4aaf2654b9d
Gil Arturo Barrios Del Villar <gil.barriosdelvillar@epfl.ch>
2024-08-06 09:04:16 +0000

    implement simplified sidebar

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -1,25 +1,105 @@
-import { useReducer, useRef } from 'react';
-import { LinkItem, LabItem, ProjectItem } from '../VerticalLinks';
-import ExpandedSideMenu from './ExpandedSideMenu';
-import CollapsedSideMenu from './CollapsedSideMenu';
+import { useAtomValue } from 'jotai';
+import { unwrap } from 'jotai/utils';
+import Link from 'next/link';
+import { DownOutlined, HomeOutlined, UpOutlined } from '@ant-design/icons';
+import { LabItem, LinkItem, ProjectItem } from '../VerticalLinks';
 
-type Props = {
-  links: LinkItem[];
+import { virtualLabDetailAtomFamily } from '@/state/virtual-lab/lab';
+import { virtualLabProjectDetailsAtomFamily } from '@/state/virtual-lab/projects';
+
+import { Role } from '@/constants/virtual-labs/sidemenu';
+import { classNames } from '@/util/utils';
+
+type SideMenuProps = {
   lab: LabItem;
+  links: LinkItem[];
   project?: ProjectItem;
 };
 
-export default function SideMenu({ links, lab, project }: Props) {
-  const ref = useRef<HTMLDivElement>(null);
-  const [expanded, toggleExpand] = useReducer((val: boolean) => !val, false);
+function ProjectLink({ project, lab }: { project: ProjectItem; lab: LabItem }) {
+  const projectInfo = useAtomValue(
+    virtualLabProjectDetailsAtomFamily({
+      virtualLabId: lab.id,
+      projectId: project.id,
+    })
+  );
 
   return (
-    <div ref={ref}>
-      {expanded ? (
-        <ExpandedSideMenu lab={lab} project={project} toggleExpand={toggleExpand} links={links} />
-      ) : (
-        <CollapsedSideMenu links={links} lab={lab} project={project} toggleExpand={toggleExpand} />
-      )}
+    projectInfo && (
+      <div className="mt-5 flex w-full flex-col items-center justify-center overflow-hidden">
+        <Link
+          key={lab.id}
+          href={lab.href}
+          className="overflow-hidden text-ellipsis whitespace-nowrap text-center font-semibold capitalize hover:text-white"
+          style={{
+            writingMode: 'vertical-rl',
+            transform: 'rotate(180deg)',
+          }}
+        >
+          {projectInfo && <DownOutlined className="my-3 text-primary-3" />}
+          {projectInfo?.name}
+        </Link>
+      </div>
+    )
+  );
+}
+
+export default function SideMenu({ lab, project, links }: SideMenuProps) {
+  const virtualLab = useAtomValue(unwrap(virtualLabDetailAtomFamily(lab.id)));
+  return (
+    <div className="relative flex h-screen w-10 flex-col items-center gap-2 bg-primary-9 text-light transition-transform ease-in-out will-change-auto">
+      <div className="h-5 w-full" />
+      <div className="flex grow flex-col items-center justify-between gap-3 overflow-hidden">
+        <div className="flex w-full flex-col gap-3 overflow-hidden">
+          {links
+            .slice()
+            .reverse()
+            .map(
+              (link) =>
+                link.role !== Role.Scale && (
+                  <>
+                    <Link
+                      key={link.key}
+                      href={link.href}
+                      className={classNames(
+                        link.styles,
+                        'w-[21px] font-semibold capitalize hover:text-white'
+                      )}
+                      style={{ writingMode: 'vertical-rl', transform: 'rotate(180deg)' }}
+                    >
+                      {link.content}
+                    </Link>
+                    <UpOutlined className="ml-1 mt-2 text-primary-3" />
+                  </>
+                )
+            )}
+          {project && <ProjectLink project={project} lab={lab} />}
+          {lab && (
+            <div className="flex w-full flex-col items-center gap-2 overflow-hidden text-primary-3">
+              <Link
+                key={lab.id}
+                href={lab.href}
+                className="overflow-hidden text-ellipsis whitespace-nowrap text-center capitalize"
+                style={{
+                  writingMode: 'vertical-rl',
+                  transform: 'rotate(180deg)',
+                }}
+              >
+                <span>
+                  Virtual lab:
+                  <span className="mt-3 inline-block text-white">{virtualLab?.name}</span>
+                </span>
+              </Link>
+            </div>
+          )}
+        </div>
+
+        <div className="mb-5 flex w-full flex-col items-center gap-2 overflow-hidden text-primary-3">
+          <Link href={lab?.href ?? '#'}>
+            <HomeOutlined />
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
