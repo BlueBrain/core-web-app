5edb505a13e2879ef90883e4bb22f858e3765627
Konstantinos Platis <konstantinos.platis@epfl.ch>
2024-08-30 07:59:45 +0000

    Merge layout for pages under /lab/<ID>

--- src/app/virtual-lab/lab/[virtualLabId]/(lab)/layout.tsx
+++ src/app/virtual-lab/lab/[virtualLabId]/(lab)/layout.tsx
@@ -1,5 +1,8 @@
 import { ErrorBoundary } from 'react-error-boundary';
-import { ReactNode } from 'react';
+import { ReactNode, Suspense } from 'react';
+import { LoadingOutlined } from '@ant-design/icons';
+import VirtualLabTopMenu from '@/components/VirtualLab/VirtualLabTopMenu';
+import VirtualLabSidebar from '@/components/VirtualLab/VirtualLabSidebar';
 import SimpleErrorComponent from '@/components/GenericErrorFallback';
 import SideMenu from '@/components/SideMenu';
 import { Label, LinkItemKey } from '@/constants/virtual-labs/sidemenu';
@@ -24,7 +27,27 @@ export default async function VirtualLabLayout({ children, params }: Props) {
             href: `/virtual-lab/lab/${params.virtualLabId}/overview`,
           }}
         />
-        {children}
+        <div className="flex h-screen w-full overflow-y-scroll bg-primary-9 p-8 text-white">
+          <ErrorBoundary FallbackComponent={SimpleErrorComponent}>
+            <div className="m-w-3/12 flex flex-row gap-4" style={{ width: '25%' }}>
+              <VirtualLabSidebar virtualLabId={params.virtualLabId} />
+            </div>
+
+            <div className="m-w-9/12 ml-3" style={{ width: '75%' }}>
+              <VirtualLabTopMenu />
+
+              <Suspense
+                fallback={
+                  <div className="flex h-full items-center justify-center">
+                    <LoadingOutlined />
+                  </div>
+                }
+              >
+                {children}
+              </Suspense>
+            </div>
+          </ErrorBoundary>
+        </div>
       </ErrorBoundary>
     </div>
   );
