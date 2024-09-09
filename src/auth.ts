f2312b90152629f0ae89ccde683aff74c112d23a
Pavlo Getta <pavlo.getta@epfl.ch>
2024-09-09 14:25:23 +0200

    Use default jwt session max age (30 days)

--- src/auth.ts
+++ src/auth.ts
@@ -124,7 +124,6 @@ export const authOptions: NextAuthOptions = {
   },
   session: {
     strategy: 'jwt',
-    maxAge: 1800,
   },
   pages: {
     signIn: basePath + '/log-in', // eslint-disable-line
