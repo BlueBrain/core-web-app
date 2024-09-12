4ff6b6506cf287c11f41cb02b0d222957f1c9adb
Konstantinos Platis <konstantinos.platis@epfl.ch>
2024-09-12 15:22:09 +0000

    Fix redirection to sandbox when no VLs

--- src/app/virtual-lab/(home)/page.tsx
+++ src/app/virtual-lab/(home)/page.tsx
@@ -1,18 +1,24 @@
+import { ReactNode } from 'react';
 import { redirect } from 'next/navigation';
-
 import VirtualLabDashboard from '@/components/VirtualLab/VirtualLabDashboard';
 import { getVirtualLabsOfUser } from '@/services/virtual-lab/labs';
 
 export default async function VirtualLabMainPage() {
+  let redirectPath: string | null = null;
+  let toRender: ReactNode = null;
   try {
     const virtualLabs = await getVirtualLabsOfUser();
-
-    if (!virtualLabs?.data?.results || virtualLabs.data.results.length === 0) {
-      redirect('/virtual-lab/sandbox/home');
+    if (!virtualLabs.data.results || virtualLabs.data.results.length === 0) {
+      redirectPath = '/virtual-lab/sandbox/home';
     }
 
-    return <VirtualLabDashboard virtualLabs={virtualLabs.data.results} />;
+    toRender = <VirtualLabDashboard virtualLabs={virtualLabs.data.results} />;
   } catch (error) {
-    return <div>An error occurred while loading the dashboard.</div>;
+    toRender = <div>An error occurred while loading the dashboard.</div>;
+  } finally {
+    if (redirectPath) {
+      redirect(redirectPath);
+    }
   }
+  return toRender;
 }
