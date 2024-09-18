1934ca58a1195b426238a217fc2be0576ba37d82
Bilal Meddah <bilal.meddah@epfl.ch>
2024-09-18 14:47:17 +0000

    Redirect to explore from build in details view

--- src/app/virtual-lab/lab/[virtualLabId]/project/[projectId]/build/page.tsx
+++ src/app/virtual-lab/lab/[virtualLabId]/project/[projectId]/build/page.tsx
@@ -9,7 +9,7 @@ import { useRouter } from 'next/navigation';
 import { DataType } from '@/constants/explore-section/list-views';
 import { Btn } from '@/components/Btn';
 import { generateVlProjectUrl } from '@/util/virtual-lab/urls';
-import { detailUrlBuilder } from '@/util/common';
+import { to64 } from '@/util/common';
 import { MODEL_DATA_TYPES } from '@/constants/explore-section/data-types/model-data-types';
 import { ExploreDataScope } from '@/types/explore-section/application';
 import { selectedSimulationScopeAtom } from '@/state/simulate';
@@ -44,13 +44,13 @@ const SupportedTypeToTabDetails: Record<string, TabDetails> = {
     title: 'Single neuron model',
     buildModelLabel: 'New model',
     newUrl: 'build/me-model/new',
-    viewUrl: 'build/me-model/view',
+    viewUrl: 'explore/interactive/model/me-model',
   },
   [DataType.SingleNeuronSynaptome]: {
     title: 'Synaptome',
     buildModelLabel: 'New synaptome model',
     newUrl: 'build/synaptome/new',
-    viewUrl: 'build/synaptome/view',
+    viewUrl: 'explore/interactive/model/synaptome',
   },
 };
 
@@ -120,8 +120,9 @@ export default function VirtualLabProjectBuildPage({ params }: Params) {
       case SimulationType.SingleNeuron:
       case SimulationType.Synaptome: {
         const vlProjectUrl = generateVlProjectUrl(params.virtualLabId, params.projectId);
-        const baseBuildUrl = `${vlProjectUrl}/${SupportedTypeToTabDetails[dataType].viewUrl}`;
-        router.push(`${detailUrlBuilder(baseBuildUrl, record)}`);
+        const pathId = `${to64(`${record._source.project.label}!/!${record._id}`)}`;
+        const baseExploreUrl = `${vlProjectUrl}/${SupportedTypeToTabDetails[dataType].viewUrl}`;
+        router.push(`${baseExploreUrl}/${pathId}`);
         break;
       }
       default:
